18966464c70a41d3c29a88a91c46836102374fde
Miguel Muscat <miguelmuscat93@gmail.com>
2014-05-10 17:30:52 +0200

    Removed JS code from HTML

--- includes/metaboxes.php
+++ includes/metaboxes.php
@@ -42,44 +42,6 @@ function edd_bk_render_booking_meta_box() {
 	$edd_bk_nonce = wp_create_nonce( basename( __FILE__ ) ); ?>
 	<input type="hidden" name="edd_bk_meta_box_nonce" value="<?php echo $edd_bk_nonce; ?>" />
 
-
-	<script type="text/javascript">
-	(function($){
-		$(document).ready( function() {
-			// TOGGLERS
-			var togglers = {
-
-				// Main toggler
-				"#edd_bk_enabled": [
-					"click",
-					function(){
-						$(".edd_bk_toggled_row").toggle( $("#edd_bk_enabled").is(":checked") );
-					}
-				],
-
-				// All day toggler
-				"#edd_bk_all_day": [
-					"click",
-					function() {
-						$("#edd_bk_start_time, #edd_bk_end_time").toggle( $("#edd_bk_all_day").is(":not(:checked)") );
-					}
-				],
-
-			};
-
-			// Initialize togglers
-			for( selector in togglers ) {
-				var toggler = togglers[selector];
-				var event = toggler[0];
-				var fn = toggler[1];
-				$(selector).on( event, fn );
-				fn();
-			}
-
-		});
-	})(jQuery);
-	</script>
-
 	<table class="form-table">
 
 		<!-- ENABLER -->
