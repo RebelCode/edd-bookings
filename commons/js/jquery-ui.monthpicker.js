676a967341dbda7cb74b5caa017cd4637eba6e9a
Miguel Muscat <miguelmuscat93@gmail.com>
2015-02-25 19:49:57 +0100

    Monthpicker inits as multiDatesPicker

--- commons/js/jquery-ui.monthpicker.js
+++ commons/js/jquery-ui.monthpicker.js
@@ -14,10 +14,27 @@
 
 	$.fn.monthPicker = function( method ) {
 		var mp_arguments = arguments;
-		var _this = this;
+		var ret = this;
 		var today_date = new Date();
 		var day_zero = new Date(0);
 		var mp_events = {};
+
+		var methods = {
+			init : function( options ) {
+				$(this).multiDatesPicker( 'init', options );
+			}
+		}
+
+		if(methods[method]) {
+			var exec_result = methods[method].apply(this, Array.prototype.slice.call(mp_arguments, 1));
+			// check method
+			return exec_result;
+		} else if( typeof method === 'object' || ! method ) {
+			return methods.init.apply(this, mp_arguments);
+		} else {
+			$.error('Method ' +  method + ' does not exist on jQuery.multiDatesPicker');
+		}
+		return false;
 	}
 
 })( jQuery );
