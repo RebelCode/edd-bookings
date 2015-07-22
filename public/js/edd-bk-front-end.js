5926436293c1374c606a1e852f79dd922f891d4b
Miguel Muscat <miguelmuscat93@gmail.com>
2015-07-22 13:43:26 +0200

    Front-end JS uses same date-checking logic as back-end

--- public/js/edd-bk-front-end.js
+++ public/js/edd-bk-front-end.js
@@ -78,57 +78,46 @@
 		$.fn[ pickerFn ].apply( datepicker_element, [options]);
 	};
 
-
 	var datepickerIsDateAvailable = function( date ) {
-		var today = new Date();
-		// Return false ifthe date has passed already
-		if (date < today && date.getDate() < today.getDate()) {
-			return [false, ''];
-		}
-		// Use the fill as default availability
+		var year = date.getFullYear();
+		var month = date.getMonth() + 1;
+		var day = date.getDate();
+		var dotw = ( ( date.getDay() + 6 ) % 7 ) + 1;
+		var week = date.getWeekNumber();
 		var available = Utils.strToBool( EDD_BK.meta.availability_fill );
-		// For each availability
-		for ( i in EDD_BK.meta.availability ) {
-			// Get the availability
-			var av = EDD_BK.meta.availability[i];
-			var range = av.type;
-			// The checking function to call, and its args
-			var fn = null;
-			var args = [];
 
-			// If the range type is a weekday
-			// Change the range type (from 'monday', 'tuesday', etc...) to 'weekday'
-			// And add the weekday as a function arg
-			var weekday = Utils.weekdayStrToInt( range );
-			if ( weekday !== -1 ) {	
-				range = 'weekday';
-				args = [weekday];
+		var finished = false;
+		for (var unit in EDD_BK.availability) {
+			var rules = EDD_BK.availability[ unit ];
+			switch (unit) {
+				case 'month':
+					if ( _.has(rules, month) ) {
+						available = rules[month];
+						finished = true;
+					}
+					break;
+				case 'week':
+					if ( _.has(rules, week) ) {
+						available = rules[week];
+						finished = true;
+					}
+					break;
+				case 'day':
+					if ( _.has(rules, dotw) ) {
+						available = rules[ dotw ];
+						finished = true;
+					}
+					break;
+				case 'custom':
+					if ( _.has(rules, [year, month, day]) ) {
+						available = _.get(rules, [year, month, day]);
+						finished = true;
+					}
+					break;
 			}
-
-			// If the checking function exists
-			// Set fn to the checking function in rangeCheckers
-			// Add the date and availability to the args, at the beginning
-			if ( range in rangeCheckers ) { 
-				fn = rangeCheckers[range];
-				args = [date, av].concat(args);
-			}
-
-			/*
-			 * Maybe some other functions can be put here?
-			 * If so, change `fn` to point to the function and `args` to an array of args
-			 */
-
-			// If the function pointer is not null,
-			// Run the function with the args. 'null' for no object context
-			// If the checking function returns TRUE, then the date matches the current range, and
-			// its availability should be deterined by that range's availability flag.
-			if ( fn !== null ) {
-				var obeys = fn.apply( null, args );
-				if ( obeys ) {
-					available = Utils.strToBool( av.available );
-				}
-			}
+			if ( finished ) break;
 		}
+
 		return [available, ''];
 	};
 
@@ -237,123 +226,6 @@
 					$('#edd-bk-datepicker').data('suppress-click-event', true).find('.ui-datepicker-current-day').first().find('>a').click();
 				});
 			}
-		}
-	};
-
-	function addTimeArrays(time1, time2) {
-		// Add the hours and minutes
-		var h = parseInt(time1[0]) + parseInt(time2[0]);
-		var m = parseInt(time1[1]) + parseInt(time2[1]);
-		// Parse the minutes into a time array (which can result in more hours)
-		var newTime = numUnitToTimeArray(m, 'minutes');
-		// New time is the added hours + carried hours, and the added minutes
-		return [h + newTime[0], newTime[1]];
-	}
-
-	function numUnitToTimeArray(num, unit) {
-		num = parseInt( num );
-		var h = ( (unit == "hours")? num: 0 );
-		var m = ( unit == "minutes" )? num % 60 : 0;
-		if ( unit == "minutes" ) h += Math.floor( num / 60 );
-		return [ h, m ];
-	}
-
-	function timeSliceFromUnit(time, unit) {
-		var i = unit == 'hours'? 0 : 1;
-		return time[i];
-	}
-
-	function timeArrayTo(unit, time) {
-		if ( unit == 'hours' ) {
-			return parseInt( time[0] );
-		}
-		else return ( parseInt( time[0] ) * 60 ) + parseInt( time[1] );
-	}
-
-	function diffTimeArray(time1, time2, format) {
-		format = format || 'array';
-		var t1 = timeArrayTo('minutes', time1);
-		var t2 = timeArrayTo('minutes', time2);
-		var diff = t1 - t2;
-		if ( format === 'minutes' ) {
-			return diff;
-		} else if ( format == 'hours' ) {
-			return diff / 60;
-		} else {
-			return numUnitToTimeArray(diff, 'minutes');
-		}
-	}
-
-
-	/**
-	 * The range checking functions.
-	 */
-	var rangeCheckers = {
-		days: function( date, av ) {
-			var day = date.getDay();
-			var from = Utils.weekdayStrToInt( av.from );
-			var to = Utils.weekdayStrToInt( av.to );
-			return day >= from && day <= to;
-		},
-
-		weeks: function( date, av ) {
-			var week = date.getWeekNumber();
-			var from = parseInt( av.from );
-			var to = parseInt( av.to );
-			return week >= from && week <= to;
-		},
-
-		months: function ( date, av ) {
-			var month = date.getMonth();
-			var from = Utils.monthStrToInt( av.from );
-			var to = Utils.monthStrToInt( av.to );
-			return month >= from && month <= to;
-		},
-
-		custom: function( date, av ){
-			var from = Utils.parseDateFromServer( av.from );
-			var to = Utils.parseDateFromServer( av.to );
-			return date >= from && date <= to;
-		},
-
-		all_week: function( date, av ) {
-			return Utils.strToBool(av.available) || (av.from.length == 0 && av.to.length == 0);
-		},
-
-		/**
-		 * Matches a particulat WHOLE weekday.
-		 * Only matches, say 10:00 - 17:00 Wednesday, if it is marked as available,
-		 * or if the times have been left out (indicating a full day match).
-		 */
-		weekday: function( date, av, weekday ) {
-			return (
-				( date.getDay() == weekday ) &&
-				( Utils.strToBool(av.available) || (av.from.length == 0 && av.to.length == 0) )
-			);
-		},
-
-		/**
-		 * Matches any WHOLE weekday.
-		 * Only matches, 15:00 - 18:00 on weekdays, if it is marked as available,
-		 * or if the times have been left out (indicating full weekdays).
-		 */
-		weekdays: function( date, av ) {
-			return (
-				( date.getDay() > 0 && date.getDay() < 6 ) &&
-				( Utils.strToBool(av.available) || (av.from.length == 0 && av.to.length == 0) )
-			);
-		},
-
-		/**
-		 * Matches any WHOLE weekend day.
-		 * Only matches, say 12:00 - 15:00 on weekends, if it is marked as available,
-		 * or if the times have been left out (indicating a full weekend days).
-		 */
-		weekend: function( date, av ) {
-			return (
-				( date.getDay() == 0 || date.getDay() == 6 ) &&
-				( Utils.strToBool(av.available) || (av.from.length == 0 && av.to.length == 0) )
-			);
 		}
 	};
 
