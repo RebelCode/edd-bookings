ab46a7ee7ac76f35e2e23901a550c44570ca5712
Miguel Muscat <miguelmuscat93@gmail.com>
2015-07-28 16:55:58 +0200

    Fixed bug in lib Customer model class

--- includes/lib/Aventura/Bookings/Customer.php
+++ includes/lib/Aventura/Bookings/Customer.php
@@ -24,7 +24,7 @@ class Aventura_Bookings_Customer extends Aventura_Bookings_Object {
 	 * 
 	 * @param string|int|array $arg The customer's ID, or an array containing the customer properties.
 	 */
-	public function __construct( $args = NULL ) {
+	public function __construct( $arg = NULL ) {
 		// If the argument is not an array, treat is as the ID
 		if ( ! is_array( $arg ) ) {
 			$arg = array( 'id' => $arg );
