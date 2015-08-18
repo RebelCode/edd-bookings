d6d030bc7c448ae3c737aa68e0ec258be27f8d58
Miguel Muscat <miguelmuscat93@gmail.com>
2015-08-18 16:22:58 +0200

    i18n readme

---
+++ languages/i18n-notes-readme.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * i18n notes:
+ *
+ * Since this plugin uses the Aventura/Bookings library for handling all booking logic, certain strings printed by the plugin may be originating
+ * from the library. These include the range names for the availability builder and certain exception messages.
+ *
+ * We are trying to find a better way to handle this for the future, but for the time being it is advised to add the strings that need
+ * to be translated manually to the translation files.
+ */
+
+// much silent, many golden. wow
