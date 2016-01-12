6c5cdc8216d67a73f74af6f01b9719971f68aff6
Miguel Muscat <miguelmuscat93@gmail.com>
2016-01-12 12:49:51 +0100

    Helper getter method for multiview download option (EDDBOOK-28)
    - Added nice-name getter method for code readability and boolean casting
    
    EDDBOOK-28 #time 5m

--- includes/downloads/class-edd-bk-download.php
+++ includes/downloads/class-edd-bk-download.php
@@ -17,5 +17,14 @@ class EDD_BK_Download extends Aventura_Bookings_Service {
 	public function isEnabled() {
 		return (bool) $this->getData('enabled') === TRUE;
 	}
+
+	/**
+	 * Checks if the Download has multi view output enabled.
+	 * 
+	 * @return boolean True if multi view output is enabled, false if not.
+	 */
+	public function isEnabledMultiViewOutput() {
+		return (bool) $this->getData('multi_view_output') === TRUE;
+	}
 	
 }
