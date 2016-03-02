5bfff3e47e8c70c9ed9eaa8d28cd724ab77666dd
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-02 19:30:10 +0100

    Created Availability model class (EDDBOOK-50)
    EDDBOOK-50 #time 1h

---
+++ includes/Aventura/Edd/Bookings/Model/Availability.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Aventura\Edd\Bookings\Model;
+
+use \Aventura\Diary\Bookable\Availability as DiaryAvailability;
+
+/**
+ * Availability model class.
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+class Availability extends DiaryAvailability
+{
+    
+    /**
+     * The ID.
+     * 
+     * @var integer
+     */
+    protected $_id;
+    
+    /**
+     * Constructs a new instance.
+     * 
+     * @param integer $id The ID.
+     */
+    protected function _construct($id)
+    {
+        $this->setId($id);
+    }
+    
+    /**
+     * Gets the ID.
+     * 
+     * @return integer The ID.
+     */
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    /**
+     * Sets the ID.
+     * 
+     * @param integer $id The ID.
+     * @return Availability This instance.
+     */
+    public function setId($id)
+    {
+        $this->_id = $id;
+        return $this;
+    }
+
+}
