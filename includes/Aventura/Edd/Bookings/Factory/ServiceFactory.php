ab5d7beec8ebcb2aad0aabb3646326a664f9c87e
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-02 18:37:24 +0100

    Created ServiceFactory class (EDDBOOK-41)
    EDDBOOK-41 #time 15m

---
+++ includes/Aventura/Edd/Bookings/Factory/ServiceFactory.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Aventura\Edd\Bookings\Factory;
+
+use \Aventura\Diary\Bookable\Availability\AvailabilityInterface;
+use \Aventura\Edd\Bookings\CustomPostType\ServicePostType;
+use \Aventura\Edd\Bookings\Plugin;
+use \Aventura\Edd\Bookings\Service;
+
+/**
+ * Service Factory class.
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+class ServiceFactory extends ModelCptFactoryAbstract
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    const DEFAULT_CLASSNAME = 'Aventura\\Edd\\Bookings\\Service';
+
+    /**
+     * The availability factory.
+     * 
+     * @var AvailabilityFactory
+     */
+    protected $_availabilityFactory;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(Plugin $plugin)
+    {
+        parent::__construct($plugin);
+        $this->setAvailabilityFactory(new AvailabilityFactory);
+    }
+
+    /**
+     * Gets the availability factory.
+     * 
+     * @return AvailabilityFactory
+     */
+    public function getAvailabilityFactory()
+    {
+        return $this->_availabilityFactory;
+    }
+
+    /**
+     * Sets the availability factory to use.
+     * 
+     * @param AvailabilityFactory $availabilityFactory The availability factory instane to use.
+     * @return ServiceFactory This instance.
+     */
+    public function setAvailabilityFactory(AvailabilityFactory $availabilityFactory)
+    {
+        $this->_availabilityFactory = $availabilityFactory;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     * 
+     * @param array $arg The data to use for instantiation.
+     * @return Service The created service instance.
+     */
+    public function create(array $arg = array())
+    {
+        if (!isset($arg['id'])) {
+            $service = null;
+        } else {
+            $data = \wp_parse_args($arg, array(
+                'session_length' => 1,
+                'session_unit' => 'hours',
+                'session_cost' => 0,
+                'min_sessions' => 1,
+                'max_sessions' => 1,
+                'multi_view_output' => false,
+                'availability' => null
+            ));
+            /* @var $availability AvailabilityInterface */
+            $availability = is_array($data['availability']) ? $this->getAvailabilityFactory()->create($data['availability']) : $this->getPlugin()->getAvailabilityController()->getAvailability($data['availability']);
+            /* @var $service Service */
+            $className = $this->getClassName();
+            $service = new $className($data['id']);
+            // Set the data and return
+            $service->setSessionLength(intval($data['session_length']))
+                    ->setSessionUnit(intval($data['session_unit']))
+                    ->setSessionCost(floatval($data['session_cost']))
+                    ->setMinSessions(intval($data['min_sessions']))
+                    ->setMaxSessions(intval($data['max_sessions']))
+                    ->setMultiViewOutput(filter_var($data['multi_view_output'], FILTER_VALIDATE_BOOLEAN))
+                    ->setAvailability($availability);
+        }
+        return $service;
+    }
+
+    /**
+     * Creates the service CPT.
+     * 
+     * @param array $data Optional array of data. Default: array()
+     * @return ServicePostType The created instance.
+     */
+    public function createCpt(array $data = array())
+    {
+        return new ServicePostType($this->getPlugin());
+    }
+
+}
