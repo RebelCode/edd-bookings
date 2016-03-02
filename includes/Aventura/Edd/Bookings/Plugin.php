e48bee50c4fc9e0cccd1bc276dfdc18b253d1cab
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-02 13:56:26 +0100

    Plugin class method for parent plugin checks (EDDBOOK-49)
    EDDBOOK-49 #time 20m

--- includes/Aventura/Edd/Bookings/Plugin.php
+++ includes/Aventura/Edd/Bookings/Plugin.php
@@ -11,28 +11,35 @@ use \Aventura\Edd\Bookings\Controller\ServiceController;
  */
 class Plugin
 {
-    
+
     /**
      * The factory that is used to create this instance and its components.
      * 
      * @var Factory
      */
     protected $_factory;
-    
+
     /**
      * Internationalization class.
      * 
      * @var I18n
      */
     protected $_i18n;
-    
+
     /**
      * The hook manager.
      * 
      * @var HookManager
      */
     protected $_hookManager;
-    
+
+    /**
+     * String used to cache the reason for deactivation.
+     * 
+     * @var string
+     */
+    protected $_deactivationReason;
+
     /**
      * Creates a new instance.
      * 
@@ -41,16 +48,13 @@ class Plugin
     public function __construct(array $data = array())
     {
         // Load the EDD license handler and create the license handler instance
-		if ( class_exists( 'EDD_License' ) ) {
-			$this->license = new \EDD_License(EDD_BK, EDD_BK_PLUGIN_NAME, EDD_BK_VERSION, EDD_BK_AUTHOR);
+        if (class_exists('EDD_License')) {
+            $this->license = new \EDD_License(EDD_BK, EDD_BK_PLUGIN_NAME, EDD_BK_VERSION, EDD_BK_AUTHOR);
         }
-        // These getters will instantiate the memebers
-        $this->getHookManager()
-            ->getI18n();
         // Define WordPress hooks
         $this->hook();
     }
-    
+
     /**
      * Gets the factory.
      * 
@@ -72,7 +76,7 @@ class Plugin
         $this->_factory = $factory;
         return $this;
     }
-    
+
     /**
      * Gets the internationalization class.
      * 
@@ -85,7 +89,7 @@ class Plugin
         }
         return $this->_i18n;
     }
-    
+
     /**
      * Gets the hook manager.
      * 
@@ -98,7 +102,57 @@ class Plugin
         }
         return $this->_hookManager;
     }
-    
+
+    /**
+     * Checks for plugin dependancies.
+     */
+    public function checkPluginDependancies()
+    {
+        if (!class_exists(EDD_BK_PARENT_PLUGIN_CLASS)) {
+            $this->deactivate('The <strong>Easy Digital Downloads</strong> plugin must be installed and activated.');
+        } else if (\version_compare(EDD_VERSION, EDD_BK_PARENT_PLUGIN_MIN_VERSION, '<')) {
+            $this->deactivate(
+                    sprintf(
+                            'The <strong>Easy Digital Downloads</strong> plugin must be at version %s or later',
+                            EDD_BK_PARENT_PLUGIN_MIN_VERSION
+                    )
+            );
+        }
+    }
+
+    /**
+     * Deactivates this plugin.
+     *
+     * @param callbable|string $arg The notice callback function (that will be hooked on `admin_notices` after
+     *                              deactivation, or a string specifying the reason for deactivation. Default: null
+     */
+    public function deactivate($arg = null)
+    {
+        // load plugins.php file from WordPress if not loaded
+        require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+        deactivate_plugins(EDD_BK_BASE);
+        if (!is_null($arg)) {
+            if (is_callable($arg)) {
+                $this->getHookManager()->addAction('admin_notices', null, $arg);
+            } else {
+                $this->_deactivationReason = $arg;
+                $this->getHookManager()->addAction('admin_notices', $this, 'showDeactivationNotice');
+            }
+        }
+    }
+
+    /**
+     * Prints an admin notice that tells the user that the plugin has been deactivated, and why.
+     *
+     * @since 1.0.0
+     */
+    public function show_deactivation_reason()
+    {
+        echo '<div class="error notice is-dismissible"><p>';
+        echo 'The <strong>EDD Bookings</strong> plugin has been deactivated. ' . $this->_deactivationReason;
+        echo '</p><button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button></div>';
+    }
+
     /**
      * Adds the WordPress hooks.
      */
