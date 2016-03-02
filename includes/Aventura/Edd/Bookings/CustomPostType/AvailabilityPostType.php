f0b5990e01dd0e346d0d86a3d089f433e876cfb3
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-02 19:31:09 +0100

    Created AvailbilityPostType class (EDDBOOK-50)
    EDDBOOK-50 #time 25m

---
+++ includes/Aventura/Edd/Bookings/CustomPostType/AvailabilityPostType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Aventura\Edd\Bookings\CustomPostType;
+
+use \Aventura\Edd\Bookings\CustomPostType;
+use \Aventura\Edd\Bookings\Plugin;
+
+/**
+ * The Availability custom post type.
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+class AvailabilityPostType extends CustomPostType
+{
+    
+    /**
+     * The CPT slug name.
+     */
+    const SLUG = 'edd_bk_availability';
+    
+    /**
+     * Constructs a new instance.
+     * 
+     * @param Plugin $plugin The parent plugin instance.
+     */
+    public function __construct(Plugin $plugin)
+    {
+        parent::__construct($plugin, self::SLUG);
+        $this->generateLabels('Availability', 'Availabilities')
+                ->setDefaultProperties();
+    }
+
+    /**
+     * Sets the properties to their default.
+     * 
+     * @return AvailabilityPostType This instance.
+     */
+    public function setDefaultProperties()
+    {
+        $properties = array(
+            'public' => false,
+            'show_ui' => true,
+            'has_archive' => false,
+            'show_in_menu' => 'edit.php?post_type=download',
+            'supports' => array('title')
+        );
+        $filtered = \apply_filters('edd_bk_availability_cpt_properties', $properties);
+        $this->setProperties($filtered);
+        return $this;
+    }
+    
+    /**
+     * Registers the WordPress hooks.
+     */
+    public function hook()
+    {
+        $this->getPlugin()->getHookManager()
+                ->addAction('admin_init', $this, 'register');
+    }
+
+}
