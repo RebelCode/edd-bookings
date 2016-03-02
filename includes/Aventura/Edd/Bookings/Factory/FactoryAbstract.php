3cededee02f8422996f60fada8dfadb8d632ded7
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-02 13:42:46 +0100

    Factory interface and abstract class (EDDBOOK-49)
    EDDBOOK-49 #time 20m

---
+++ includes/Aventura/Edd/Bookings/Factory/FactoryAbstract.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Aventura\Edd\Bookings\Factory;
+
+use \Aventura\Edd\Bookings\Plugin;
+
+/**
+ * Basic customizable single-class generator factory.
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+abstract class FactoryAbstract implements FactoryInterface
+{
+    
+    /**
+     * Default class name to use.
+     */
+    const DEFAULT_CLASSNAME = '';
+    
+    /**
+     * The name of the class to instantiate.
+     * 
+     * @var string
+     */
+    protected $_className;
+    
+    /**
+     * The parent plugin.
+     * 
+     * @var Plugin
+     */
+    protected $_plugin;
+    
+    /**
+     * Constructs a new instance.
+     * 
+     * @param Plugin $plugin The parent plugin instance.
+     */
+    public function __construct(Plugin $plugin)
+    {
+        $this->setClassName(static::DEFAULT_CLASSNAME);
+    }
+    
+    /**
+     * Gets the name of the class to instantiate.
+     * 
+     * @return string
+     */
+    public function getClassName()
+    {
+        return $this->_className;
+    }
+    
+    /**
+     * Sets the name of the class to instantiate.
+     * 
+     * @param string $className The name of the class to instantiate.
+     * @return FactoryAbstract This instance.
+     */
+    public function setClassName($className)
+    {
+        $this->_className = $className;
+        return $this;
+    }
+    
+    /**
+     * {@inheritdoc}
+     * 
+     * @return Plugin
+     */
+    public function getPlugin()
+    {
+        return $this->_plugin;
+    }
+
+    /**
+     * Sets the parent plugin instance.
+     * 
+     * @param Plugin $plugin The parent plugin instance.
+     * @return FactoryAbstract This instance.
+     */
+    public function setPlugin(Plugin $plugin)
+    {
+        $this->_plugin = $plugin;
+        return $this;
+    }
+
+}
