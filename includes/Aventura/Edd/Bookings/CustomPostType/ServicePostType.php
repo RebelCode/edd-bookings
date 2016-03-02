ef2b6509aaa4fe1388ddf667500fc8c242742df2
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-02 18:35:12 +0100

    Created ServicePostType class (EDDBOOK-41)
    EDDBOOK-41 #time 10m

---
+++ includes/Aventura/Edd/Bookings/CustomPostType/ServicePostType.php
@@ -0,0 +1,39 @@
+<?php
+
+use \Aventura\Edd\Bookings\CustomPostType;
+use \Aventura\Edd\Bookings\Plugin;
+
+namespace Aventura\Edd\Bookings\CustomPostType;
+
+/**
+ * Service Custom Post Type class.
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+class ServicePostType extends CustomPostType
+{
+    
+    /**
+     * The CPT slug name.
+     */
+    const SLUG = 'download';
+    
+    /**
+     * Constructs a new instance.
+     * 
+     * @param Plugin $plugin The parent plugin instance.
+     */
+    public function __construct(Plugin $plugin)
+    {
+        parent::__construct($plugin, self::SLUG);
+    }
+
+    /**
+     * Regsiters the WordPress hooks.
+     */
+    public function hook()
+    {
+        // Do nothing. The service CPT is actually the Download CPT from EDD
+    }
+
+}
