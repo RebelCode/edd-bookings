7d8207add14fa67e4434ed38504960c43d705bba
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-03 12:59:14 +0100

    Rewrote autoload file to unbind it from the library (EDDBOOK-49)
    EDDBOOK-49 #time 30m

--- includes/autoload.php
+++ includes/autoload.php
@@ -1,39 +1,25 @@
 <?php
 
-if (!function_exists('diaryAutoloader')) {
-    /**
-     * Gets the Diary library's autoloader singleton instance.
-     * 
-     * @return Aventura\Diary\Autoloader
-     */
-    function diaryAutoloader()
-    {
-        /* @var $instance Aventura\Diary\Autoloader */
-        static $instance = null;
-
-        if (is_null($instance)) {
-            $instance = newDiaryAutoloader();
-            $instance->register();
-        }
-
-        return $instance;
-    }
-}
-
-if (!function_exists('newDiaryAutoloader')) {
-    /**
-     * Creates a new Diary autoloader instance.
-     * 
-     * @return Aventura\Diary\Autoloader
-     */
-    function newDiaryAutoloader()
-    {
-        $className = 'Aventura\\Diary\\Autoloader';
+/**
+ * Gets the autoloader singleton instance.
+ * 
+ * @return Aventura\Autoloader
+ */
+function eddBookingsAutoloader()
+{
+    /* @var $instance Aventura\Autoloader */
+    static $instance = null;
+    // If null, instantiate
+    if (is_null($instance)) {
+        $className = 'Aventura\\Autoloader';
         if (!class_exists($className)) {
             $dir = dirname(__FILE__);
-            $classPath = sprintf('%1$s/%2$s.php', $dir, str_replace('\\', DIRECTORY_SEPARATOR, $className) );
+            $classNameAsPath = str_replace('\\', DIRECTORY_SEPARATOR, $className);
+            $classPath = sprintf('%1$s%2$s%3$s.php', $dir, DIRECTORY_SEPARATOR, $classNameAsPath);
             require_once $classPath;
         }
-        return new $className();
+        $instance = new $className();
+        $instance->register();
     }
+    return $instance;
 }
