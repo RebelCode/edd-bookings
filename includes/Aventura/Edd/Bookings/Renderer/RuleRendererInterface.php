844fe0fe1c1eab65e6c658f203af2717956d2f6b
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-03 20:04:26 +0100

    Added renderer base classes (EDDBOOK-52)
    EDDBOOK-52 #time 20m

---
+++ includes/Aventura/Edd/Bookings/Renderer/RuleRendererInterface.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Aventura\Edd\Bookings\Renderer;
+
+use \Aventura\Diary\Bookable\Availability\Timetable\Rule\RangeRuleAbstract;
+
+/**
+ * An object that can render a rule.
+ */
+interface RuleRendererInterface
+{
+
+    /**
+     * Constructs a new instance.
+     * 
+     * @param RangeRuleAbstract $rule The rule to render.
+     */
+    public function __construct(RangeRuleAbstract $rule);
+
+    /**
+     * Gets the rule name.
+     * 
+     * @return string
+     */
+    public function getRuleName();
+
+    /**
+     * Renders the rule's range start option.
+     * 
+     * @param array $data Optional array of data. Accepted entries are: <br/>
+     *                    "name" to specify the HTML name attribute <br/>
+     *                    "class" to specify the HTML class attribute <br/>
+     *                    Default: <b>array()</b>
+     * @return string
+     */
+    public function renderRangeStart(array $data = array());
+
+    /**
+     * Renders the rule's range end option.
+     * 
+     * @param array $data Optional array of data. Accepted entries are: <br/>
+     *                    "name" to specify the HTML name attribute <br/>
+     *                    "class" to specify the HTML class attribute <br/>
+     *                    Default: <b>array()</b>
+     * @return string
+     */
+    public function renderRangeEnd(array $data = array());
+
+    /**
+     * Renders the rule's available option.
+     * 
+     * @param array $data Optional array of data. Accepted entries are: <br/>
+     *                    "name" to specify the HTML name attribute <br/>
+     *                    "class" to specify the HTML class attribute <br/>
+     *                    Default: <b>array()</b>
+     * @return string
+     */
+    public function renderAvailable(array $data = array());
+
+}
