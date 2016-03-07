c39ee2d5a37ec8c3b0cc139478cc799ef3dd6a83
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-07 19:22:37 +0100

    Created service js and css files (EDDBOOK-41)
    EDDBOOK-41 #time 30m

---
+++ assets/js/service.js
@@ -0,0 +1,29 @@
+function eddBkService(element) {
+    $ = jQuery;
+    element = $(element);
+    
+    // Toggles the sections based on whether bookings are enabled
+    var updateSectionVisibility = function() {
+        var bookingsEnabled = element.find('input#edd-bk-bookings-enabled').is(':checked');
+        element.find('div.edd-bk-service-section:not(:first-child)').toggle(bookingsEnabled);
+        // Also hide some other metaboxes
+        var edd_metaboxes_to_hide = [
+            '#edd_product_prices',
+            '#edd_product_files',
+            '#edd_product_settings'
+        ];
+        $(edd_metaboxes_to_hide.join(',')).toggle(!bookingsEnabled);
+    };
+    
+    // When the bookings are enabled/disabled, update the section visibility
+    element.find('input#edd-bk-bookings-enabled').change(updateSectionVisibility);
+    // Check section visibility on first run
+    updateSectionVisibility();
+}
+
+// Initializes all service containers
+jQuery(document).ready(function() {
+    jQuery('div.edd-bk-service-container').each(function() {
+        eddBkService(this);
+    });
+});
