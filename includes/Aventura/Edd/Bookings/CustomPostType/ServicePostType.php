6c9e8933fdb976467a1b37e992e44caa0a2e6bc5
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-07 19:22:02 +0100

    Service post type adds metabox and uses renderer (EDDBOOK-41)
    EDDBOOK-41 #time 15m

--- includes/Aventura/Edd/Bookings/CustomPostType/ServicePostType.php
+++ includes/Aventura/Edd/Bookings/CustomPostType/ServicePostType.php
@@ -4,6 +4,7 @@ namespace Aventura\Edd\Bookings\CustomPostType;
 
 use \Aventura\Edd\Bookings\CustomPostType;
 use \Aventura\Edd\Bookings\Plugin;
+use \Aventura\Edd\Bookings\Renderer\ServiceRenderer;
 
 /**
  * Service Custom Post Type class.
@@ -12,12 +13,12 @@ use \Aventura\Edd\Bookings\Plugin;
  */
 class ServicePostType extends CustomPostType
 {
-    
+
     /**
      * The CPT slug name.
      */
     const SLUG = 'download';
-    
+
     /**
      * Constructs a new instance.
      * 
@@ -29,11 +30,35 @@ class ServicePostType extends CustomPostType
     }
 
     /**
+     * Registers the WordPress metaboxes for this cpt.
+     */
+    public function addMetaBoxes()
+    {
+        \add_meta_box('edd-bk-service', __('Booking Options', $this->getPlugin()->getI18n()->getDomain()),
+                array($this, 'renderServiceMetabox'), static::SLUG, 'normal', 'high');
+    }
+
+    /**
+     * Renders the service metabox.
+     * 
+     * @param WP_Post $post The post.
+     */
+    public function renderServiceMetabox($post)
+    {
+        $service = (empty($post->ID))
+                ? $this->getPlugin()->getServiceController()->getFactory()->create(array('id' => 0))
+                : $this->getPlugin()->getServiceController()->get($post->ID);
+        $renderer = new ServiceRenderer($service);
+        echo $renderer->render();
+    }
+
+    /**
      * Regsiters the WordPress hooks.
      */
     public function hook()
     {
-        // Do nothing. The service CPT is actually the Download CPT from EDD
+        $this->getPlugin()->getHookManager()
+                ->addAction('add_meta_boxes', $this, 'addMetaboxes');
     }
 
 }
