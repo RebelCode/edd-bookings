283e15d3655b53bb03e4e98afe52c01119705052
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-07 15:32:14 +0100

    Availability metabox rendering and meta saving (EDDBOOK-50)
    EDDBOOK-50 #time 50m

--- includes/Aventura/Edd/Bookings/CustomPostType/AvailabilityPostType.php
+++ includes/Aventura/Edd/Bookings/CustomPostType/AvailabilityPostType.php
@@ -4,6 +4,7 @@ namespace Aventura\Edd\Bookings\CustomPostType;
 
 use \Aventura\Edd\Bookings\CustomPostType;
 use \Aventura\Edd\Bookings\Plugin;
+use \Aventura\Edd\Bookings\Renderer\AvailabilityRenderer;
 
 /**
  * The Availability custom post type.
@@ -12,12 +13,12 @@ use \Aventura\Edd\Bookings\Plugin;
  */
 class AvailabilityPostType extends CustomPostType
 {
-    
+
     /**
      * The CPT slug name.
      */
     const SLUG = 'edd_bk_availability';
-    
+
     /**
      * Constructs a new instance.
      * 
@@ -31,6 +32,48 @@ class AvailabilityPostType extends CustomPostType
     }
 
     /**
+     * Registers the metaboxes.
+     */
+    public function addMetaboxes()
+    {
+        $textDomain = $this->getPlugin()->getI18n()->getDomain();
+        \add_meta_box('edd-bk-availability-options', __('Options', $textDomain), array($this, 'renderOptionsMetabox'),
+                static::SLUG, 'normal', 'core');
+    }
+
+    /**
+     * Renders the metabox.
+     */
+    public function renderOptionsMetabox($post)
+    {
+        $avaailability = (empty($post->ID)) ? $this->getPlugin()->getAvailabilityController()->getFactory()->create(array(
+                        'id' => 0)) : $this->getPlugin()->getAvailabilityController()->get($post->ID);
+        $renderer = new AvailabilityRenderer($avaailability);
+        echo $renderer->render();
+    }
+
+    public function onSave($postId, $post) {
+        if ($this->_guardOnSave($postId, $post)) {
+            check_admin_referer('edd_bk_save_meta', 'edd_bk_availability');
+            // Save the download meta
+            $meta = $this->extractMeta();
+            $this->getPlugin()->getAvailabilityController()->saveMeta($postId, $meta);
+        }
+    }
+    
+    public function extractMeta() {
+        // Filter input post data
+        $timetableId = filter_input(INPUT_POST, 'edd-bk-availability-timetable-id', FILTER_VALIDATE_INT);
+        // Generate meta
+        $meta = array(
+                'timetable_id'  =>  $timetableId
+        );
+        // Filter and return
+        $filtered = \apply_filters('edd_bk_availability_saved_meta', $meta);
+        return $filtered;
+    }
+    
+    /**
      * Sets the properties to their default.
      * 
      * @return AvailabilityPostType This instance.
@@ -38,24 +81,26 @@ class AvailabilityPostType extends CustomPostType
     public function setDefaultProperties()
     {
         $properties = array(
-            'public' => false,
-            'show_ui' => true,
-            'has_archive' => false,
-            'show_in_menu' => 'edit.php?post_type=download',
-            'supports' => array('title')
+                'public'       => false,
+                'show_ui'      => true,
+                'has_archive'  => false,
+                'show_in_menu' => 'edit.php?post_type=download',
+                'supports'     => array('title')
         );
         $filtered = \apply_filters('edd_bk_availability_cpt_properties', $properties);
         $this->setProperties($filtered);
         return $this;
     }
-    
+
     /**
      * Registers the WordPress hooks.
      */
     public function hook()
     {
         $this->getPlugin()->getHookManager()
-                ->addAction('init', $this, 'register');
+                ->addAction('init', $this, 'register')
+                ->addAction('add_meta_boxes', $this, 'addMetaboxes')
+                ->addAction('save_post', $this, 'onSave', 10, 2);
     }
 
 }
