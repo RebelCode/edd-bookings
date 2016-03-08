945237cb606655da8c1571150ebbf293efc1f824
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-08 18:21:29 +0100

    Added All Week time rule renderer (EDDBOOK-52)
    EDDBOOK-52 #time 10m

---
+++ includes/Aventura/Edd/Bookings/Timetable/Rule/Renderer/AllWeekTimeRangeRenderer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Aventura\Edd\Bookings\Timetable\Rule\Renderer;
+
+use \Aventura\Diary\DateTime\Day;
+
+/**
+ * Description of AllWeekTimeRangeRenderer
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+class AllWeekTimeRangeRenderer extends DotwGroupTimeRangeRendererAbstract
+{
+    
+    const CLASSNAME = 'AllWeekTimeRule';
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getRuleName()
+    {
+        return __('All Week', eddBookings()->getI18n()->getDomain());
+    }
+
+}
