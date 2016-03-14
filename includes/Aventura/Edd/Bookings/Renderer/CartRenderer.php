426eb714433c66344815fc8c46686adcf5a7ecc5
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-14 12:32:55 +0100

    Created CartRenderer class (EDDBOOK-53)
    EDDBOOK-53 #time 1h 20m

---
+++ includes/Aventura/Edd/Bookings/Renderer/CartRenderer.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Aventura\Edd\Bookings\Renderer;
+
+use \Aventura\Diary\DateTime;
+use \Aventura\Diary\DateTime\Duration;
+use \Aventura\Diary\DateTime\Period;
+
+/**
+ * Renders the EDD Cart data.
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+class CartRenderer extends RendererAbstract
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render(array $data = array())
+    {
+        // Get item
+        $item = $this->getObject();
+        // Get the service
+        $id = $item['id'];
+        $service = eddBookings()->getServiceController()->get($id);
+        // Get text domain
+        $textDomain = eddBookings()->getI18n()->getDomain();
+        // Get item options
+        $itemOptions = $item['options'];
+        // Prepare output var
+        $output = '';
+        // If bookings enabled and item data has booking info
+        if ($service->getBookingsEnabled() && isset($itemOptions['edd_bk'])) {
+            // Get item booking info
+            $bookingInfo = $itemOptions['edd_bk'];
+            $start = new DateTime(intval($bookingInfo['start']));
+            $duration = new Duration(intval($bookingInfo['duration']));
+            $timezone = new Duration(intval($bookingInfo['timezone']));
+            $start->plus($timezone);
+            // Create period
+            $period = new Period($start, $duration);
+            // Get date and time formats
+            $dateformat = get_option('date_format', 'd/m/y');
+            $timeformat = get_option('time_format', 'H:i');
+            $datetimeFormat = sprintf('%s %s', $dateformat, $timeformat);
+            // Output
+            ob_start();
+            ?>
+            <p class="edd-bk-cart-booking-start">
+                <?php _e('Start:', $textDomain); ?>
+                <em><?php echo $period->getStart()->format($datetimeFormat); ?></em>
+            </p>
+            <p class="edd-bk-cart-booking-end">
+                <?php _e('End:', $textDomain); ?>
+                <em><?php echo $period->getEnd()->format($datetimeFormat); ?></em>
+            </p>
+            <?php
+            $output = ob_get_clean();
+        }
+        $filteredOutput = apply_filters('edd_bk_cart_item_output', $output, $item, $service);
+        return $filteredOutput;
+    }
+
+}
