b91df7ca3d5c9aaa4c39aae563d1491e984be8e8
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-14 18:22:11 +0100

    Documenting AssetsController (EDDBOOK-55)
    EDDBOOK-55 #time 15m

--- includes/Aventura/Edd/Bookings/Controller/AssetsController.php
+++ includes/Aventura/Edd/Bookings/Controller/AssetsController.php
@@ -3,17 +3,45 @@
 namespace Aventura\Edd\Bookings\Controller;
 
 /**
- * Assets controller class, for registering and enqueueing static assets.
+ * This class is responsible for registering and enqueueing static asset files, such as stylesheets, scripts and fonts.
+ * 
+ * On it's own, this class will enqueue assets on three instances:
+ *  AssetsController#commonAssets() is called on every page load
+ *  AssetsController#backendAssets() is called on the backend
+ *  AssetsController#frontendAssets() is called on the frontend
+ * 
+ * However, the registration/enqueueing methods may be used externally, given that they are called at the appropriate
+ * point in time (on the correct WP hook).
+ * 
+ * @version 1.0.0
+ * @since [*next-version*]
  */
 class AssetsController extends ControllerAbstract
 {
 
-    // Asset type constants
+    /**
+     * @constant A script-type asset.
+     */
     const TYPE_SCRIPT = 'script';
+    
+    /**
+     * @constant A style-type asset.
+     */
     const TYPE_STYLE = 'style';
-    // Asset hooks constants
+    
+    /**
+     * @constant The WP hook used to register/enqueue assets on the frontend.
+     */
     const HOOK_FRONTEND = 'wp_enqueue_scripts';
+    
+    /**
+     * @constant The WP hook used to register/enqueue assets on the backend.
+     */
     const HOOK_ADMIN = 'admin_enqueue_scripts';
+    
+    /**
+     * @constant The WP hook used to register/enqueue assets on the login page.
+     */
     const HOOK_LOGIN = 'login_enqueue_scripts';
 
     /**
@@ -37,7 +65,6 @@ class AssetsController extends ControllerAbstract
     public function commonAssets()
     {
         $this->enqueueStyle('font-awesome', EDD_BK_CSS_URL . 'font-awesome.min.css');
-
         return $this;
     }
 
@@ -55,10 +82,10 @@ class AssetsController extends ControllerAbstract
         // Our datepicker skin
         $this->enqueueStyle('edd-bk-datepicker-css', EDD_BK_CSS_URL . 'datepicker-skin.css',
                 array('jquery-ui-style-css'));
-        
+
         // Out frontend styles
         $this->enqueueStyle('edd-bk-service-frontend-css', EDD_BK_CSS_URL . 'service-frontend.css');
-        
+
         // Mutltidatepicker addon
         $this->enqueueScript('jquery-ui-multidatepicker', EDD_BK_JS_URL . 'jquery-ui.multidatespicker.js',
                 array('jquery', 'jquery-ui-core', 'jquery-ui-datepicker'), '1.6.3');
@@ -93,8 +120,8 @@ class AssetsController extends ControllerAbstract
     /**
      * Registers a script.
      *
-     * @uses Assets::script()
-     * @see Assets::script()
+     * @uses AssetsController::script()
+     * @see AssetsController::script()
      */
     public function registerScript($handle, $src, $deps = array(), $ver = false, $in_footer = false)
     {
@@ -104,8 +131,8 @@ class AssetsController extends ControllerAbstract
     /**
      * Enqueues a script.
      *
-     * @uses Assets::script()
-     * @see Assets::script()
+     * @uses AssetsController::script()
+     * @see AssetsController::script()
      */
     public function enqueueScript($handle, $src = null, $deps = array(), $ver = false, $in_footer = false)
     {
@@ -121,7 +148,7 @@ class AssetsController extends ControllerAbstract
      * @param  array   $deps      An array of script handles that this script depends upon. Default: array()
      * @param  boolean $ver       The version of the script. Default: false
      * @param  boolean $in_footer If true, the script is added to the footer of the page. If false, it is added to the document head. Default: false
-     * @return Aventura\Edd\AddToCartPopup\Core\AssetsController
+     * @return AssetsController
      */
     protected function script($enqueue, $handle, $src = null, $deps = array(), $ver = false, $in_footer = false)
     {
@@ -131,8 +158,8 @@ class AssetsController extends ControllerAbstract
     /**
      * Registers a style.
      *
-     * @uses Assets::style()
-     * @see Assets::style()
+     * @uses AssetsController::style()
+     * @see AssetsController::style()
      */
     public function registerStyle($handle, $src, $deps = array(), $ver = false, $media = 'all')
     {
@@ -142,8 +169,8 @@ class AssetsController extends ControllerAbstract
     /**
      * Enqueues a style.
      *
-     * @uses Assets::style()
-     * @see Assets::style()
+     * @uses AssetsController::style()
+     * @see AssetsController::style()
      */
     public function enqueueStyle($handle, $src = null, $deps = array(), $ver = false, $media = 'all')
     {
@@ -159,7 +186,7 @@ class AssetsController extends ControllerAbstract
      * @param  array   $deps    An array of style handles that this style depends upon. Default: array()
      * @param  boolean $ver     The version of the style. Default: false
      * @param  string  $media   The style's media scope. Default: all
-     * @return Aventura\Edd\AddToCartPopup\Core\AssetsController
+     * @return AssetsController
      */
     public function style($enqueue, $handle, $src, $deps = array(), $ver = false, $media = 'all')
     {
@@ -176,18 +203,17 @@ class AssetsController extends ControllerAbstract
      * @param  array   $deps    Array of other similar asset handles that this asset depends on.
      * @param  string  $ver     String version of the asset, for caching purposes.
      * @param  mixed   $extra   Extra data to be included, such as style media or script location in document.
-     * @return Aventura\Edd\AddToCartPopup\Core\AssetsController
+     * @return AssetsController
      */
     protected function handleAsset($type, $enqueue, $handle, $src, $deps, $ver, $extra)
     {
         // Generate name of function to use (whether for enqueueing or registration)
-        $fn = sprintf('\wp_%1$s_%2$s',
-                $enqueue === true
-                        ? 'enqueue'
-                        : 'register', $type);
+        $enqueueOrRegister = ($enqueue === true)
+                ? 'enqueue'
+                : 'register';
+        $fn = sprintf('\wp_%1$s_%2$s', $enqueueOrRegister, $type);
         // Call the enqueue/register function
         call_user_func_array($fn, array($handle, $src, $deps, $ver, $extra));
-
         return $this;
     }
 
