27babf37de9a5745cfdd390294537f3f0c5ff3f5
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-16 16:03:38 +0100

    Removed backend timetable script dependancy (EDDBOOK-55)
    EDDBOOK-55 #time 5m

--- includes/Aventura/Edd/Bookings/Controller/AssetsController.php
+++ includes/Aventura/Edd/Bookings/Controller/AssetsController.php
@@ -107,7 +107,7 @@ class AssetsController extends ControllerAbstract
     public function backendAssets()
     {
         $this->enqueueStyle('edd-bk-timetable-css', EDD_BK_CSS_URL . 'timetable.css');
-        $this->enqueueScript('edd-bk-timetable-js', EDD_BK_JS_URL . 'timetable.js', array('edd-bk-service-frontend'));
+        $this->enqueueScript('edd-bk-timetable-js', EDD_BK_JS_URL . 'timetable.js');
         $this->enqueueStyle('jquery-ui-timepicker-css', EDD_BK_CSS_URL . 'jquery-ui-timepicker.css');
         $this->enqueueScript('jquery-ui-timepicker-addon', EDD_BK_JS_URL . 'jquery-ui-timepicker.js',
                 array('jquery-ui-datepicker'));
