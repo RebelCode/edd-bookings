36f69120eb7c870c7e0d0789e0e3a5f36f91c043
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-18 15:24:55 +0100

    Added placeholder text for main page (EDDBOOK-56)
    EDDBOOK-56 #time 20m

--- includes/Aventura/Edd/Bookings/Renderer/MainPageRenderer.php
+++ includes/Aventura/Edd/Bookings/Renderer/MainPageRenderer.php
@@ -131,14 +131,19 @@ class MainPageRenderer extends \Aventura\Edd\Bookings\Renderer\RendererAbstract
         $textDomain = $this->getTextDomain();
         return \apply_filters('edd_bk_mainpage_tabs',
                 array(
-                static::tab('', __("What's New", $textDomain), function() {
-                    return 'hi';
-                }),
-                static::tab('gs', __('Getting Started', $textDomain)),
-                static::tab('docs', __('Documentation', $textDomain)),
-                static::tab('changelog', __('Changelog', $textDomain)),
+                static::tab('', __("What's New", $textDomain), array($this, 'loremIpsum')),
+                static::tab('gs', __('Getting Started', $textDomain), array($this, 'loremIpsum')),
+                static::tab('docs', __('Documentation', $textDomain), array($this, 'loremIpsum')),
+                static::tab('changelog', __('Changelog', $textDomain), array($this, 'loremIpsum')),
                 )
         );
+    }
+    
+    public function loremIpsum()
+    {
+        return '<p>Bacon ipsum dolor amet shoulder beef strip steak ut, elit pancetta ham labore in id. Dolore spare ribs jowl capicola pork loin ipsum, anim porchetta exercitation culpa salami strip steak. Duis tongue tri-tip, turducken venison beef shankle incididunt aliquip t-bone sunt cillum picanha. Et pork loin sed bresaola, ipsum leberkas spare ribs magna in ut ground round.</p>'
+            . '<p>Prosciutto short ribs ex porchetta chuck laboris salami cillum dolore shoulder occaecat. Shankle magna bacon velit, cow cupim dolor aliquip flank doner. Swine dolore jerky biltong shoulder. Prosciutto nulla jerky incididunt hamburger strip steak pork loin ut bacon kielbasa laborum nisi esse mollit doner. Doner turkey pastrami filet mignon, jerky leberkas brisket hamburger t-bone irure pork belly beef ribs tongue ground round. Incididunt beef ad, andouille meatball in drumstick ut aute et ut swine nisi mollit spare ribs.</p>'
+            . '<p>Exercitation est laboris adipisicing, ipsum laborum nulla ut tongue porchetta quis ham hock rump. Brisket anim kevin, sunt ut frankfurter irure jerky duis chicken ham hock ea biltong. Non meatloaf andouille, tenderloin capicola ham hock eiusmod. Flank cupim consequat venison short loin hamburger. Shoulder pancetta in, fatback sirloin voluptate sint occaecat excepteur pork chop laboris beef ribs. In nostrud landjaeger quis commodo pork fatback t-bone nisi. Jowl laboris est incididunt short ribs, shoulder cow.</p>';
     }
     
     /**
