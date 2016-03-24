2ff5a9900d7bceb27fc1b619be2579df9342f619
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-24 19:32:23 +0100

    Calendar page renderer (EDDBOOK-56)
    EDDBOOK-56 #time 1h

---
+++ includes/Aventura/Edd/Bookings/Renderer/CalendarPageRenderer.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Aventura\Edd\Bookings\Renderer;
+
+/**
+ * Description of CalendarPageRenderer
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+class CalendarPageRenderer extends RendererAbstract
+{
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function render(array $data = array())
+    {
+        $plugin = $this->getObject();
+        $textDomain = $plugin->getI18n()->getDomain();
+        ob_start();
+        ?>
+        <div class="wrap">
+            <h1><i class="fa fa-calendar"></i> <?php _e('Calendar', $textDomain); ?></h1>
+            <hr/>
+            <div class="edd-bk-admin-calendar"></div>
+            <script type="text/javascript">
+                (function($) {
+                    $(document).ready(function() {
+                        $('div.edd-bk-admin-calendar').fullCalendar({
+                            deafaultView: 'week',
+                            header: {
+                                left: 'today',
+                                center: 'agendaDay,agendaWeek,month',
+                                right: 'prev,next'
+                            },
+                            views: {
+                                basic: {},
+                                agenda: {},
+                                week: {},
+                                day: {}
+                            },
+                            aspectRatio: 2.2,
+                            eventSources: [
+                                {
+                                    url: window.ajaxurl,
+                                    type: 'POST',
+                                    data: {
+                                        action: 'edd_bk_get_bookings_for_calendar'
+                                    }
+                                }
+                            ]
+                        });
+                    });
+                })(jQuery);
+            </script>
+        </div>
+        <?php
+        return ob_get_clean();
+    }
+
+}
