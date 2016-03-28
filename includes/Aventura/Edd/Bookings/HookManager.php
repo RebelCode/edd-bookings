72d2c011a03d814bf0077ffa94fa89e14bfe5687
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-28 15:13:11 +0200

    HookManager method docs cleanup (EDDBOOK-55)
    EDDBOOK-55 #time 10m

--- includes/Aventura/Edd/Bookings/HookManager.php
+++ includes/Aventura/Edd/Bookings/HookManager.php
@@ -36,11 +36,11 @@ class HookManager
     /**
      * Add a new action to the collection to be registered with WordPress.
      *
-     * @var      string               $hook             The name of the WordPress action that is being registered.
-     * @var      object               $component        A reference to the instance of the object on which the action is defined.
-     * @var      string               $callback         The name of the function definition on the $component.
-     * @var      int      Optional    $priority         The priority at which the function should be fired.
-     * @var      int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
+     * @param      string   $hook             The name of the WordPress action that is being registered.
+     * @param      object   $component        A reference to the instance of the object on which the action is defined.
+     * @param      string   $callback         The name of the function definition on the $component.
+     * @param      int      $priority         The priority at which the function should be fired.
+     * @param      int      $accepted_args    The number of arguments that should be passed to the $callback.
      * @return HookManager This instance.
      */
     public function addAction($hook, $component, $callback, $priority = 10, $accepted_args = 1)
@@ -53,11 +53,11 @@ class HookManager
      * Add a new filter to the collection to be registered with WordPress.
      *
      * @since    1.0.0
-     * @var      string               $hook             The name of the WordPress filter that is being registered.
-     * @var      object               $component        A reference to the instance of the object on which the filter is defined.
-     * @var      string               $callback         The name of the function definition on the $component.
-     * @var      int      Optional    $priority         The priority at which the function should be fired.
-     * @var      int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
+     * @param      string   $hook             The name of the WordPress filter that is being registered.
+     * @param      object   $component        A reference to the instance of the object on which the filter is defined.
+     * @param      string   $callback         The name of the function definition on the $component.
+     * @param      int      $priority         The priority at which the function should be fired.
+     * @param      int      $accepted_args    The number of arguments that should be passed to the $callback.
      * @return HookManager This instance.
      */
     public function addFilter($hook, $component, $callback, $priority = 10, $accepted_args = 1)
@@ -70,12 +70,12 @@ class HookManager
      * A utility function that is used to register the actions and hooks into a single
      * collection.
      *
-     * @var      array                $hooks            The collection of hooks that is being registered (that is, actions or filters).
-     * @var      string               $hook             The name of the WordPress filter that is being registered.
-     * @var      object               $component        A reference to the instance of the object on which the filter is defined.
-     * @var      string               $callback         The name of the function definition on the $component.
-     * @var      int      Optional    $priority         The priority at which the function should be fired.
-     * @var      int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
+     * @param      array                $hooks            The collection of hooks that is being registered (that is, actions or filters).
+     * @param      string               $hook             The name of the WordPress filter that is being registered.
+     * @param      object               $component        A reference to the instance of the object on which the filter is defined.
+     * @param      string               $callback         The name of the function definition on the $component.
+     * @param      int      Optional    $priority         The priority at which the function should be fired.
+     * @param      int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
      * @return   type                                   The collection of actions and filters registered with WordPress.
      */
     public function add($hooks, $hook, $component, $callback, $priority, $accepted_args)
