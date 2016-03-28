ae6db1df3f6c5a91ab4e2a518614234edccf4764
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-28 17:52:07 +0200

    BookingsCalendarRenderer accepts args (EDDBOOK-56)
    Args control output of wrap element, header and data attributes.
    
    EDDBOOK-56 #time 30m

--- includes/Aventura/Edd/Bookings/Renderer/BookingsCalendarRenderer.php
+++ includes/Aventura/Edd/Bookings/Renderer/BookingsCalendarRenderer.php
@@ -13,18 +13,34 @@ class BookingsCalendarRenderer extends RendererAbstract
     /**
      * {@inheritdoc}
      */
-    public function render(array $data = array())
+    public function render(array $pArgs = array())
     {
         $plugin = $this->getObject();
         $textDomain = $plugin->getI18n()->getDomain();
+        // Parse args
+        $defaultArgs = array(
+                'wrap'   => true,
+                'header' => true,
+                'data'   => array()
+        );
+        $args = wp_parse_args($pArgs, $defaultArgs);
+        // Prepare data attr
+        $dataAttrs = '';
+        foreach ($args['data'] as $_key => $_value) {
+            $dataAttrs .= sprintf(' data-%s="%s"', $_key, esc_attr($_value));
+        }
         ob_start();
-        ?>
-        <div class="wrap">
-            <h1><i class="fa fa-calendar"></i> <?php _e('Calendar', $textDomain); ?></h1>
-            <hr/>
-            <div class="edd-bk-bookings-calendar"></div>
-        </div>
-        <?php
+        if ($args['wrap']) {
+            echo '<div class="wrap">';
+        }
+        if ($args['header']) {
+            printf('<h1><i class="fa fa-calendar"></i> %s</h1>', __('Calendar', $textDomain));
+            echo '<hr/>';
+        }
+        printf('<div class="edd-bk-bookings-calendar" %s></div>', $dataAttrs);
+        if ($args['wrap']) {
+            echo '</div>';
+        }
         return ob_get_clean();
     }
 
