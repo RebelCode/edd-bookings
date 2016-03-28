766519cbe5bd5483aadd9e5d502c5ff7630a616e
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-28 17:33:32 +0200

    Removed old inline calendar page script (EDDBOOK-56)
    EDDBOOK-56 #time 10m

--- includes/Aventura/Edd/Bookings/Renderer/BookingsCalendarRenderer.php
+++ includes/Aventura/Edd/Bookings/Renderer/BookingsCalendarRenderer.php
@@ -22,37 +22,7 @@ class BookingsCalendarRenderer extends RendererAbstract
         <div class="wrap">
             <h1><i class="fa fa-calendar"></i> <?php _e('Calendar', $textDomain); ?></h1>
             <hr/>
-            <div class="edd-bk-admin-calendar"></div>
-            <script type="text/javascript">
-                (function($) {
-                    $(document).ready(function() {
-                        $('div.edd-bk-admin-calendar').fullCalendar({
-                            deafaultView: 'week',
-                            header: {
-                                left: 'today prev,next',
-                                center: 'title',
-                                right: 'agendaDay,agendaWeek,month'
-                            },
-                            views: {
-                                basic: {},
-                                agenda: {},
-                                week: {},
-                                day: {}
-                            },
-                            aspectRatio: 2.2,
-                            eventSources: [
-                                {
-                                    url: window.ajaxurl,
-                                    type: 'POST',
-                                    data: {
-                                        action: 'edd_bk_get_bookings_for_calendar'
-                                    }
-                                }
-                            ]
-                        });
-                    });
-                })(jQuery);
-            </script>
+            <div class="edd-bk-bookings-calendar" data-schedule="772"></div>
         </div>
         <?php
         return ob_get_clean();
