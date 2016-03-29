f5e4e7ec86a9a695f90a6224dacc6b2e37d61005
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-29 13:52:21 +0200

    Added meta prefix for service meta (EDDBOOK-59)
    - Controller saves meta with prefix
    - Controller gets meta with prefix
    - Controller uses prefix in queries
    
    EDDBOOK-59 #time 40m

--- includes/Aventura/Edd/Bookings/Controller/ServiceController.php
+++ includes/Aventura/Edd/Bookings/Controller/ServiceController.php
@@ -13,6 +13,8 @@ use \Aventura\Edd\Bookings\Model\Service;
 class ServiceController extends ModelCptControllerAbstract
 {
 
+    const META_PREFIX = 'edd_bk_';
+    
     /**
      * Gets a single service by ID.
      * 
@@ -71,7 +73,7 @@ class ServiceController extends ModelCptControllerAbstract
         // Prepare query args
         $metaQueries = array();
         $metaQueries[] = array(
-                'key'     => 'availability_id',
+                'key'     => $this->metaPrefix('availability_id'),
                 'value'   => $id,
                 'compare' => (is_array($id) ? 'IN' : '=')
         );
@@ -100,12 +102,28 @@ class ServiceController extends ModelCptControllerAbstract
      */
     public function getMeta($id, array $data = array())
     {
-        $meta = parent::getMeta($id);
-        $legacy = isset($meta['edd_bk'])? $meta['edd_bk'] : null;
+        $allMeta = parent::getMeta($id);
+        $legacy = isset($allMeta['edd_bk'])
+                ? maybe_unserialize($allMeta['edd_bk'])
+                : null;
         $final = array();
-        // If meta found, set final to meta
-        if (is_array($meta)) {
-            $final = $meta;
+        // Get meta with prefix - run regex on all meta keys. preg_grep works on array values, so we use array_keys()
+        // to generate a number-indexed array containing all key strings and work with that. Returned array will be
+        // number-indexed as [i] => [key string]
+        $metaKeys = preg_grep(sprintf('/^%s/', static::META_PREFIX), array_keys($allMeta));
+        // To get the meta array, we use array_intersect_key(), which gets all elements with the same key in both param
+        // arrays. Two arrays used are the allMeta array (assoc) and the flip of the preg_grep returned array.
+        $meta = array_intersect_key($allMeta, array_flip($metaKeys));
+        // If the meta array is populated ...
+        if (is_array($meta) && count($meta) > 0) {
+            // Generate the final array.
+            // We now need to remove the prefixes so we can pass the proper data to the factory.
+            $prefixLength = strlen(static::META_PREFIX);
+            $finalKeys = array_map(function($item) use ($prefixLength) {
+                return substr($item, $prefixLength);
+            }, array_keys($meta));
+            // Generate the final array
+            $final = array_combine($finalKeys, array_values($meta));
         } elseif (is_array($legacy)) {
             // Otherwise, add legacy meta if found
             $final['legacy'] = $legacy;
@@ -121,8 +139,19 @@ class ServiceController extends ModelCptControllerAbstract
         unset($data['id']);
         $filtered = \apply_filters('edd_bk_save_service_meta', $data, $id);
         foreach ($data as $key => $value) {
-            \update_post_meta($id, $key, $value);
+            \update_post_meta($id, $this->metaPrefix($key), $value);
         }
     }
 
+    /**
+     * Prepends the meta prefix to the given meta key.
+     * 
+     * @param string $key The key.
+     * @return string The key, prepended with the meta prefix.
+     */
+    public function metaPrefix($key)
+    {
+        return sprintf('%s%s', static::META_PREFIX, $key);
+    }
+    
 }
