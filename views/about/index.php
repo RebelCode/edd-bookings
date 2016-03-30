c836126daf4744b20d8fca19532e1f51fb89e621
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-30 18:13:44 +0200

    Updated What's New page content and added i18n(EDDBOOK-57)
    EDDBOOK-57 #time 1h

--- views/about/index.php
+++ views/about/index.php
@@ -1,15 +1,27 @@
+<?php
+$textDomain = eddBookings()->getI18n()->getDomain();
+?>
 <div class="feature-section">
-    <h3>Update notice for v1.0.3 or earlier</h3> 
+    <h3 class="important-red">
+        <i class="fa fa-warning"></i>
+            <?php _e('Important update notice for v1.0.3 or earlier', $textDomain); ?>
+    </h3> 
     <div class="two-col-text">
         <p>
-            If you're updating from EDD Bookings v1.0.3 or earlier, you'll find that we've updated your Downloads and
-            created an Availability and Timetable for each of your downloads that have bookings enabled, to preserve
-            their functionality on your site. <i>Nothing's changed, but everything's new!</i>
+            <?php
+            _e('<b>This is a major update!</b> If you\'re updating from EDD Bookings v1.0.3 or earlier, you\'ll find
+            that we\'ve updated your settings for Downloads that had bookings enabled. We\'ve also auto created a
+            Schedule and a Timetable for each of these downloads to preserve their functionality on your site.<br/>
+            <i>Nothing\'s changed, but everything\'s new!</i>', $textDomain);
+            ?>
         </p>
         <p>
-            Your calendar builder settings have been converted into Timetables, each of which is assigned to its own
-            Availability. We still encourage you to go over your settings and tweak them to your needs. This will help
-            you familiarise yourself with the new features.
+            <?php
+            _e('Each of your Downloads\' calendar builder settings have been converted into Timetables, each of which is
+            assigned to its own Schedule. However, we <span class="important-red">highly recommend</span> that you go
+            over your Timetable settings to confirm their correctness. Some of the Calendar Builder rules now behave
+            differently, so you might want to tweak them to your needs.', $textDomain);
+            ?>
         </p>
     </div>
 </div>
@@ -17,7 +29,7 @@
 <hr/>
 
 <div class="headline-feature feature-section one-col">
-    <h2>Availabilities &amp; Timetables</h2>
+    <h2><?php _e('Schedules &amp; Timetables', $textDomain); ?></h2>
     <div class="media-container">
         <img src="" />
     </div>
@@ -35,15 +47,19 @@
         </center>
     </div>
     <div class="col">
-        <h3>Availabilities</h3>
+        <h3><?php _e('Schedules', $textDomain); ?></h3>
         <p>
-            Your downloads now no longer have their own calendar settings and bookings. Instead, your downloads
-            now have an <strong>availability</strong>, which is virtually identical to a real-life diary. Bookings
-            made for that download are recorded in its availability.
+            <?php
+            _e('Your downloads now no longer have their own calendar settings and bookings. Instead, your downloads
+            now have a <strong>Schedule</strong>, which is virtually identical to a real-life diary. Bookings
+            made for a download are recorded in its schedule.', $textDomain);
+            ?>
         </p>
         <p>
-            You can also set up multiple downloads to use the same availability, so that booked times for one download
-            cannot be booked for another.
+            <?php
+            _e('You can also set up multiple downloads to use the same schedule, so that booked times for one
+            download cannot be booked for another download that uses the same schedule.', $textDomain);
+            ?>
         </p>
     </div>
 </div>
@@ -57,15 +73,20 @@
         </center>
     </div>
     <div class="col">
-        <h3>Timetables</h3>
+        <h3><?php _e('Timetables', $textDomain); ?></h3>
         <p>
-            A timetable is exactly what the name says it is: a definition of the dates and times your customers are
-            allowed to book. This was previously known as the Calendar Builder, found in the download's Edit page.
-            If you've used EDD Bookings v1.0.3 or earlier, then this will be familiar territory.
+            <?php
+            _e(
+            'A timetable is exactly what the name says it is: a definition of the dates and times your customers are
+            allowed to book. This was previously known as the Calendar Builder in the download\'s Edit page.
+            If you\'ve used EDD Bookings v1.0.3 or earlier, then this will be familiar territory.', $textDomain);
+            ?>
         </p>
         <p>
-            Each one of your availabilities is linked to a timetable, and multiple availabilities can share a single
-            timetable!
+            <?php
+            _e('Each one of your schedules is linked to a timetable, and multiple schedules can share a single
+            timetable!', $textDomain);
+            ?>
         </p>
     </div>
 </div>
@@ -74,28 +95,37 @@
 
 <div class="changelog">
     <div class="feature-section under-the-hood three-col">
-        <h3>Roadmap</h3>
+        <h3><?php _e('Roadmap', $textDomain); ?></h3>
         <div class="col">
-            <h4>Quality of Life Improvements</h4>
+            <h4><?php _e('Quality of Life Improvements', $textDomain); ?></h4>
             <p>
-                A calendar preview for the Timetable editor is in the works, to reflect changes while you edit. We're
-                also looking to make a few adjustments to the interface, making it more intuitive.
+                <?php
+                _e('A calendar preview for the Timetable editor is in the works, to reflect changes while you edit.
+                We\'re also looking to make a few adjustments to the interface, making it more intuitive.', $textDomain);
+                ?>
             </p>
         </div>
         <div class="col">
-            <h4>Booking Handling</h4>
+            <h4><?php _e('Booking Handling', $textDomain); ?></h4>
             <p>
-                Cancelling bookings, placing bookings manually from the backend and reporting of bookings are all
-                planned for future versions. Our aim is to make your WordPress Admin feel like a booking system.
+                <?php
+                _e('Cancelling bookings, approving and confirmed placed bookings, placing bookings manually from the
+                backend, reporting of bookings and emailing to clients are all planned for future versions.
+                Our aim is to make your WordPress Admin feel like a booking system.', $textDomain);
+                ?>
             </p>
         </div>
         <div class="col">
-            <h4>Frontend Submissions Integration</h4>
+            <h4><?php _e('Frontend Submissions Integration', $textDomain); ?></h4>
             <p>
                 <a href="https://easydigitaldownloads.com/downloads/frontend-submissions/" target="_blank">
-                    Frontend Submissions
-                </a> is an EDD extension that turns your site into a complete marketplace. We're looking to integrate
-                EDD Bookings with FES so that your users can create bookable downloads and manage their bookings.
+                    <?php _e('Frontend Submissions', $textDomain); ?>
+                </a>
+                <?php
+                _e('is an EDD extension that turns your site into a complete marketplace. We\'re looking to
+                integrate EDD Bookings with FES so that your users can create bookable downloads and manage their
+                bookings.', $textDomain);
+                ?>
             </p>
         </div>
     </div>
