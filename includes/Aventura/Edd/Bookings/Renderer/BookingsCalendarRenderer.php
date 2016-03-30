c64af972f89824cd9254c359a5b4e5d54703e4d6
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-30 14:13:22 +0200

    Added infopane rendering to calender renderer (EDDBOOK-56)
    - New static method renders infopane
    - New render option to toggle output of infopane
    - Improved output
    
    EDDBOOK-56 #time 30m

--- includes/Aventura/Edd/Bookings/Renderer/BookingsCalendarRenderer.php
+++ includes/Aventura/Edd/Bookings/Renderer/BookingsCalendarRenderer.php
@@ -9,7 +9,7 @@ namespace Aventura\Edd\Bookings\Renderer;
  */
 class BookingsCalendarRenderer extends RendererAbstract
 {
-    
+
     /**
      * {@inheritdoc}
      */
@@ -19,9 +19,10 @@ class BookingsCalendarRenderer extends RendererAbstract
         $textDomain = $plugin->getI18n()->getDomain();
         // Parse args
         $defaultArgs = array(
-                'wrap'   => true,
-                'header' => true,
-                'data'   => array()
+                'wrap'     => true,
+                'header'   => true,
+                'infopane' => true,
+                'data'     => array()
         );
         $args = wp_parse_args($pArgs, $defaultArgs);
         // Prepare data attr
@@ -30,18 +31,50 @@ class BookingsCalendarRenderer extends RendererAbstract
             $dataAttrs .= sprintf(' data-%s="%s"', $_key, esc_attr($_value));
         }
         ob_start();
-        if ($args['wrap']) {
-            echo '<div class="wrap">';
-        }
+        // Show header if enabled in args
         if ($args['header']) {
-            printf('<h1><i class="fa fa-calendar"></i> %s</h1>', __('Calendar', $textDomain));
-            echo '<hr/>';
+            printf('<h1><i class="fa fa-calendar"></i> %s</h1><hr/>', __('Calendar', $textDomain));
         }
+        // Show calendar 
         printf('<div class="edd-bk-bookings-calendar" %s></div>', $dataAttrs);
-        if ($args['wrap']) {
-            echo '</div>';
+        // Show infopane if enabled in args
+        if ($args['infopane']) {
+            echo '<hr/>';
+            echo static::renderInfoPane();
         }
-        return ob_get_clean();
+        // Add the wrap if enabled in args
+        $innerOutput = ob_get_clean();
+        $outerOutput = $args['wrap']
+                ? sprintf('<div class="wrap">%s</div>', $innerOutput)
+                : $innerOutput;
+        // Return
+        return $outerOutput;
+    }
+    
+    /**
+     * Renders the info pane.
+     * 
+     * @param array $args Optional array of arguments. Accepted args are:<br/>
+     *                    * header - Prints a "Booking Info" header<br/>
+     * @return string The rendered HTML output.
+     */
+    public static function renderInfoPane($pArgs = array())
+    {
+        $textDomain = eddBookings()->getI18n()->getDomain();
+        $defaultArgs = array(
+                'header'   => true
+        );
+        $args = wp_parse_args($pArgs, $defaultArgs);
+        ob_start();
+        ?>
+        <div class="edd-bk-bookings-calendar-info-pane">
+            <?php if ($args['header']): ?>
+                <h4><?php _e('Booking Info'); ?></h4>
+            <?php endif; ?>
+            <div><span><?php _e('Click on a booking to view its details', $textDomain); ?></span></div>
+        </div>
+        <?php
+        return apply_filters('edd_bk_booking_info_pane_output', ob_get_clean());
     }
 
 }
