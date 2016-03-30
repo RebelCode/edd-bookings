c53903db5ff0a64c4a2032c861cf21eaaa948744
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-30 18:52:42 +0200

    Bookings calendar date selection disabled (EDDBOOK-55)
    EDDBOOK-55 #time 5m

--- assets/js/bookings-calendar.js
+++ assets/js/bookings-calendar.js
@@ -25,7 +25,6 @@
         var _this = this;
         var fullCalendarArgs = $.extend({
             defaultView: 'month',
-            selectable: true,
             header: {
                 left: 'today prev,next',
                 center: 'title',
