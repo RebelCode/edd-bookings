0973e4c1960ef1ef6c2de89490c7303a2c3912aa
Mark Zahra <mark@wpmayor.com>
2016-03-30 14:14:55 +0200

    Added info and images to Getting Started tab in About section. (EDDBOOK-57)

--- views/about/getting-started.php
+++ views/about/getting-started.php
@@ -1,1 +1,84 @@
-<p>Someone should really get started on this page...</p>
+<div class="headline-feature feature-section one-col">
+    <h2>How to Create Your First Booking</h2>
+
+    <h4>This is a basic instroduction on how to use EDD Bookings.
+
+        For more details please <a href="http://docs.easydigitaldownloads.com/category/1100-bookings" target="_blank">visit our full documentation</a>.
+    </h4>
+</div>
+
+
+    <div class="col">
+    	<h2>Adding a Bookable Download</h2>
+            <img src="<?php echo EDD_BK_IMGS_URL; ?>booking-options.png" />
+        <h4>Step 1</h4>
+        <p>
+            To create your first Booking head over to "Downloads" > "Add New". After entering the required information for the Booking's title, description, categories, tags and so on, you can get right down to the Booking details.
+        
+            Upon opening the New Download page, scroll down to the "Booking Options" metabox and check the "Enable bookings for this download" checkbox. This will automatically remove all the unrequired metaboxes and display the new Bookings settings.
+        </p>
+ 
+        <h4>Step 2</h4>
+        <p>
+            Start by selecting the session length, the number of sessions each customer can book, as well as the cost per session.
+      
+            Once the above details have been filled in you can move on to "Schedule". This automatically selects an option titled "Create new schedule and timetable."
+      
+            Leave this setting as is. The schedule and timetable will automatically be created for you and we can later change the titles and details of the newly created schedule and timetable. 
+       
+            Last but not least you can choose whether to display the calendar in a multi-view page or post.
+
+        <h4>Step 3</h4>
+        <p>
+            Once the above details have been filled in you can move on to "Schedule". This automatically selects an option titled "Create new schedule and timetable."
+       
+            Leave this setting as is. The schedule and timetable will automatically be created for you and we can later change the titles and details of the newly created schedule and timetable. 
+        </p>
+<br>
+<hr>
+<br>
+<div class="feature-section one-col">
+            <h2>Adding and Editing Schedules</h2>
+
+    
+    <div class="two-col-text">
+        <h4>Adding a Schedule</h4>
+        <p>
+            Head over to the "Schedules" section of EDD Bookings and click on "Add New" or open the automatically created schedule for your first booking.
+            
+            Here you will see two metaboxes. The first one is titled "Options" and lists the Timetable that is associated with this Schedule.
+            
+            Below this is a list of the "Downloads using this schedule" and the associated bookings.
+        </p>
+        <img src="<?php echo EDD_BK_IMGS_URL; ?>add-new-schedule.png" />
+    </div>
+    <div>
+        <h4>Editing a Schedule</h4>
+        <br>
+        <img src="<?php echo EDD_BK_IMGS_URL; ?>edit-schedule.png" />
+    </div>
+<br>
+<br>
+<hr>
+<br>
+<div class="feature-section one-col">
+            <h2>Adding and Editing Timetables</h2>
+     <div class="two-col-text">
+        <h4>Adding a Timetable</h4>
+        <p>
+            Next head on to the "Timetables" section and once again you can either create a new timetable or edit the one that was automatically created with your first booking.
+
+            Within each Timetable you'll be able to set the Rules for this timetable and also view the Schedules currently making use of this timetable and its rules.
+
+            You can find more information on how to set up the Timetable Rules <a href="#" target="_blank">in our documentation</a>.
+        </p>
+        <br>
+        <img src="<?php echo EDD_BK_IMGS_URL; ?>add-new-timetable.png" />
+    </div>
+     <div>
+        <h4>Editing a Timetable</h4>
+        <br>
+        <img src="<?php echo EDD_BK_IMGS_URL; ?>edit-timetable.png" />
+    </div>
+
+</div>
