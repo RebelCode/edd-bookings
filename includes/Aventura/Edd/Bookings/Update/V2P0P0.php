cc7f677cbc2252815978d9f3f46e9fab5c1b35fb
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-31 20:08:24 +0200

    Created sample update class for v2.0.0 (EDDBOOK-59)
    EDDBOOK-59 #time 10m

---
+++ includes/Aventura/Edd/Bookings/Update/V2P0P0.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Aventura\Edd\Bookings\Update;
+
+/**
+ * Update class for version 2.0.0
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+class V2P0P0 implements UpdateInterface
+{
+    
+    /**
+     * {@inheritdoc}
+     */
+    public static function update($previousVersion)
+    {
+        return false;
+    }
+
+}
