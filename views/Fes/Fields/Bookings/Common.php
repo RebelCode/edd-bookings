e3667172f3c61c19ae86ce2b7318aadc1a42d8e8
Miguel Muscat <miguelmuscat93@gmail.com>
2016-07-25 11:27:07 +0200

    Added session length frontend options (EDDBOOK-81)
    EDDBOOK-81 #time 20m

--- views/Fes/Fields/Bookings/Common.php
+++ views/Fes/Fields/Bookings/Common.php
@@ -1,6 +1,18 @@
 <?php
 
+use \Aventura\Diary\DateTime\Duration;
+
 $bookingsEnabled = $data['meta']['bookings_enabled'];
+$sessionUnit = $data['meta']['session_unit'];
+$sessionLengthMeta = intval($data['meta']['session_length']);
+$singleSessionLength = Duration::$sessionUnit(1, false);
+$sessionLength = $sessionLengthMeta / $singleSessionLength;
+$sessionUnits = array(
+    'minutes' => __('minutes', 'eddbk'),
+    'hours'   => __('hours', 'eddbk'),
+    'days'    => __('days', 'eddbk'),
+    'weeks'   => __('weeks', 'eddbk'),
+);
 
 if (boolval($data['options']['bookings_enabled']['enabled'])):
     ?>
@@ -11,5 +23,26 @@ if (boolval($data['options']['bookings_enabled']['enabled'])):
             value="on" <?= checked($bookingsEnabled, true) ?>
             />
             <?= $data['options']['bookings_enabled']['label'] ?>
+    </label>
+<?php endif; ?>
+
+<?php if (boolval($data['options']['session_length']['enabled'])): ?>
+    <label>
+        <input
+            type="number"
+            name="<?= $data['name'] ?>[session_length]"
+            value="<?= $sessionLength ?>"
+            />
+        <select>
+            <?php foreach ($sessionUnits as $key => $val) : ?>
+                <option
+                    value='<?= $key ?>'
+                    <?= selected($key, $sessionUnit, false) ?>
+                    >
+                    <?= $val ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+        <?= $data['options']['session_length']['label'] ?>
     </label>
 <?php endif; ?>
