1db0e0ad68771219401cf3f6451368754eefe1f4
Miguel Muscat <miguelmuscat93@gmail.com>
2016-07-28 17:22:53 +0200

    FesIntegration supports dashboard pages (EDDBOOK-87)
    EDDBOOK-87 #time 10m

--- includes/Aventura/Edd/Bookings/Integration/Fes/FesIntegration.php
+++ includes/Aventura/Edd/Bookings/Integration/Fes/FesIntegration.php
@@ -3,6 +3,8 @@
 namespace Aventura\Edd\Bookings\Integration\Fes;
 
 use \Aventura\Edd\Bookings\Integration\Core\IntegrationAbstract;
+use \Aventura\Edd\Bookings\Integration\Fes\Dashboard\DashboardPageInterface;
+use \Aventura\Edd\Bookings\Plugin;
 
 /**
  * Integration for the FrontEnd Submissions extension.
@@ -13,12 +15,53 @@ class FesIntegration extends IntegrationAbstract
 {
 
     /**
+     * The dashboard pages.
+     * 
+     * @var DashboardPageInterface[]
+     */
+    protected $dashboardPages;
+
+    /**
+     * Constructs a new instance.
+     * 
+     * @param Plugin $plugin The parent plugin instance.
+     */
+    public function __construct(Plugin $plugin = null)
+    {
+        parent::__construct($plugin);
+        $this->dashboardPages = array();
+    }
+
+    /**
+     * Gets the dashboard pages.
+     * 
+     * @return array The dashboard pages.
+     */
+    public function getDashboardPages()
+    {
+        return $this->dashboardPages;
+    }
+
+    /**
+     * Adds a dashboard page.
+     * 
+     * @param DashboardPageInterface $dashboardPage The page to add.
+     * @return FesIntegration This instance.
+     */
+    public function addDashboardPage(DashboardPageInterface $dashboardPage)
+    {
+        $this->dashboardPages[] = $dashboardPage;
+        return $this;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function hook()
     {
         $this->getPlugin()->getHookManager()
             ->addAction('fes_load_fields_require', $this, 'init');
+        return $this;
     }
 
     /**
@@ -35,6 +78,10 @@ class FesIntegration extends IntegrationAbstract
             return;
         }
         add_filter('fes_load_fields_array', array($this, 'registerFields'));
+        foreach ($this->getDashboardPages() as $page) {
+            $page->hook();
+        }
+        return $this;
     }
 
     /**
