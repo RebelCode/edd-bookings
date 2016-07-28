e6cdf85c11a740920b0c75cebd3f7b1f425c3643
Miguel Muscat <miguelmuscat93@gmail.com>
2016-07-28 17:24:05 +0200

    Created BookingsPage (EDDBOOK-87)
    EDDBOOK-87 #time 5m

---
+++ includes/Aventura/Edd/Bookings/Integration/Fes/Dashboard/BookingsPage.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Aventura\Edd\Bookings\Integration\Fes\Dashboard;
+
+/**
+ * The Bookings page for the frontend FES dashboard.
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+class BookingsPage extends DashboardPageAbstract
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render()
+    {
+    }
+
+}
