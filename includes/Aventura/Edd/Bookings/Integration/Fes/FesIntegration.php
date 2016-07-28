5f527fdf9b1d06a2f55c53abd38fa5a01c2606c7
Miguel Muscat <miguelmuscat93@gmail.com>
2016-07-28 13:30:58 +0200

    Added FES page utils methods (EDDBOOK-78)
    EDDBOOK-78 #time 20m

--- includes/Aventura/Edd/Bookings/Integration/Fes/FesIntegration.php
+++ includes/Aventura/Edd/Bookings/Integration/Fes/FesIntegration.php
@@ -73,4 +73,48 @@ class FesIntegration extends IntegrationAbstract
         return defined('fes_plugin_version') && version_compare(fes_plugin_version, 2.3, '>=');
     }
 
+    /**
+     * Checks if the current page is an FES admin page.
+     * 
+     * @return boolean
+     */
+    public static function isFesAdminPage()
+    {
+        if (!fes_is_admin()) {
+            return false;
+        }
+
+        $screen = \get_current_screen();
+        if (!is_object($screen)) {
+            return false;
+        }
+
+        $postTypes = array('fes-forms', 'download');
+        $screens = array('profile', 'user-edit', 'user', 'user-new');
+
+        return (isset($screen->base) && substr($screen->base, 0, 7) === 'edd-fes') ||
+            (isset($screen->post_type) && in_array($screen->post_type, $postTypes)) ||
+            (isset($screen->id) && in_array($screen->id, $screens));
+    }
+
+    /**
+     * Gets the FES frontend page ID.
+     * 
+     * @return string
+     */
+    public static function getFesFrontendPage()
+    {
+        return EDD_FES()->helper->get_option('fes-vendor-dashboard-page', false);
+    }
+
+    /**
+     * Checks if the current page is an FES frontend page.
+     * 
+     * @return boolean
+     */
+    public static function isFesFrontendPage()
+    {
+        return fes_is_frontend() && is_page(static::getFesFrontendPage());
+    }
+
 }
