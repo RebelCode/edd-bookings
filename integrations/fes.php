02dc03b6703a1750d4d65d184e52700c7279c627
Miguel Muscat <miguelmuscat93@gmail.com>
2016-07-29 12:56:10 +0200

    Registered edit page to FES (EDDBOOK-87)
    EDDBOOK-87 #time 10m

--- integrations/fes.php
+++ integrations/fes.php
@@ -1,6 +1,7 @@
 <?php
 
 use \Aventura\Edd\Bookings\Integration\Fes\Dashboard\BookingsPage;
+use \Aventura\Edd\Bookings\Integration\Fes\Dashboard\EditBookingPage;
 use \Aventura\Edd\Bookings\Integration\Fes\FesIntegration;
 
 // Integration instance
@@ -9,6 +10,8 @@ $fes = new FesIntegration(eddBookings());
 // Dashboard pages
 $bookingsPage = new BookingsPage(eddBookings(), 'bookings', __('Bookings', 'eddbk'), 'calendar');
 $fes->addDashboardPage($bookingsPage);
+$editBookingPage = new EditBookingPage(eddBookings(), 'edit-booking', null);
+$fes->addDashboardPage($editBookingPage);
 
 // Register the integration
 eddBookings()->addIntegration('fes', $fes);
