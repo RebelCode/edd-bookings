5500923b6c472428589a2e3b90a525085f4e084a
Miguel Muscat <miguelmuscat93@gmail.com>
2016-08-01 16:49:01 +0200

    Removed wrap and header from frontend FES calendar (EDDBOOK-87)
    EDDBOOK-87 #time 5m

--- views/Fes/Dashboard/Bookings/Calendar.php
+++ views/Fes/Dashboard/Bookings/Calendar.php
@@ -3,4 +3,7 @@
 use \Aventura\Edd\Bookings\Renderer\BookingsCalendarRenderer;
 
 $renderer = new BookingsCalendarRenderer(eddBookings());
-echo $renderer->render();
+echo $renderer->render(array(
+    'wrap'   => false,
+    'header' => false,
+));
