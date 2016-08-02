27aaeda482bf3a66594eb1bbb6dccfa360f78953
Miguel Muscat <miguelmuscat93@gmail.com>
2016-08-02 12:47:33 +0200

    Removed details link from bookings FES details page (EDDBOOK-87)
    EDDBOOK-87 #time 5m

--- views/Fes/Dashboard/Bookings/Edit.php
+++ views/Fes/Dashboard/Bookings/Edit.php
@@ -7,18 +7,12 @@
     $booking = eddBookings()->getBookingController()->get($data['booking_id']);
     $customer = new EDD_Customer($booking->getCustomerId());
     $permalink = get_permalink();
-    /*
-      <h3><?php echo $booking->getStart(); ?></h3>
-      <p>
-      <strong><?php _e('Customer: ', 'eddbk'); ?></strong>
-      <?php echo $customer->name; ?>
-      </p>
-     */
     $renderer = new \Aventura\Edd\Bookings\Renderer\BookingRenderer($booking);
     echo $renderer->render(array(
         'service_link'      => add_query_arg(array('task' => 'edit-product', 'post_id' => $booking->getServiceId()), $permalink),
         'payment_link'      => add_query_arg(array('task' => 'edit-order', 'order_id' => $booking->getPaymentId()), $permalink),
         'customer_link'     => null,
+        'view_details_link' => null,
         'table_class'       => 'table fes-table table-condensed table-striped',
     ));
     ?>
