d1edf090b64b13b99d67d4e3ddbba3363f2490ff
Miguel Muscat <miguelmuscat93@gmail.com>
2016-08-04 12:36:36 +0200

    Reorderd FES dashboard menu pages (EDDBOOK-87)
    EDDBOOK-87 #time 10m

--- integrations/fes.php
+++ integrations/fes.php
@@ -8,11 +8,15 @@ use \Aventura\Edd\Bookings\Integration\Fes\FesIntegration;
 // Integration instance
 $fes = new FesIntegration(eddBookings());
 
-// Dashboard pages
-$bookingsPage = new BookingsPage(eddBookings(), 'bookings', __('Bookings', 'eddbk'), 'book');
-$fes->addDashboardPage($bookingsPage);
+// Calendar Page
 $bookingsCalendarPage = new BookingsCalendarPage(eddBookings(), 'bookings-calendar', __('Calendar', 'eddbk'), 'calendar');
 $fes->addDashboardPage($bookingsCalendarPage);
+
+// Bookings List Page
+$bookingsPage = new BookingsPage(eddBookings(), 'bookings', __('Bookings', 'eddbk'), 'book');
+$fes->addDashboardPage($bookingsPage);
+
+// Edit Booking Page / Bookings Details Page
 $editBookingPage = new EditBookingPage(eddBookings(), 'edit-booking', null);
 $fes->addDashboardPage($editBookingPage);
 
