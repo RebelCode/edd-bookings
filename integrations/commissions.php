bd06e1d9f82e96083ccc99cbe2134672f914992f
Miguel Muscat <miguelmuscat93@gmail.com>
2016-08-12 15:18:09 +0200

    Booking template tag class for commissions email (EDDBOOK-91)
    EDDBOOK-91 #time 1h

--- integrations/commissions.php
+++ integrations/commissions.php
@@ -1,30 +1,14 @@
 <?php
 
 use \Aventura\Edd\Bookings\Integration\Commissions\CommissionsIntegration;
-use \Aventura\Edd\Bookings\Integration\Commissions\Notifications\TemplateTag;
-use \Aventura\Edd\Bookings\Model\Booking;
+use \Aventura\Edd\Bookings\Integration\Commissions\Notifications\BookingTag;
 
+// Initialize integration
 $commissions = new CommissionsIntegration(eddBookings());
 
-$bookingTemplateTag = new TemplateTag(
-    'booking',
-    __('The booked date and time, if applicable.', 'eddk'),
-    function($downloadId, $commissionId) {
-        $paymentId = get_post_meta($commissionId, '_edd_commission_payment_id', true);
-        $bookings = eddBookings()->getBookingController()->getBookingsForPayemnt($paymentId);
-        $replacement = '';
-        foreach ($bookings as $booking) {
-            /* @var $booking Booking */
-            $service = eddBookings()->getServiceController()->get($booking->getServiceId());
-            $format = $service->isSessionUnit('hours', 'minutes')
-                ? sprintf('%s %s', get_option('time_format'), get_option('date_format'))
-                : get_option('date_format');
-            $replacement .= sprintf('%s\n', $booking->format($format));
-        }
-        return $replacement;
-    }
-);
+// Booking tag for emails
+$bookingTag = new BookingTag(__('The booked date and time, if applicable.', 'eddk'));
+$commissions->addTemplateTag($bookingTag);
 
-$commissions->addTemplateTag($bookingTemplateTag);
-
+// Register integration
 eddBookings()->addIntegration('commissions', $commissions);
