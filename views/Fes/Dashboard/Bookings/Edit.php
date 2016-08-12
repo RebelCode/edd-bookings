becb88ecd4bf77a460d4979457596bd49e95c91f
Miguel Muscat <miguelmuscat93@gmail.com>
2016-08-12 13:09:19 +0200

    Disabling FES orders hides payment link on booking info page(EDDBOOK-93)
    EDDBOOK-93 #time 10m

--- views/Fes/Dashboard/Bookings/Edit.php
+++ views/Fes/Dashboard/Bookings/Edit.php
@@ -8,9 +8,12 @@
     $customer = new EDD_Customer($booking->getCustomerId());
     $permalink = get_permalink();
     $renderer = new \Aventura\Edd\Bookings\Renderer\BookingRenderer($booking);
+    $paymentLink = EDD_FES()->vendors->vendor_can_view_orders()
+        ? add_query_arg(array('task' => 'edit-order', 'order_id' => $booking->getPaymentId()), $permalink)
+        : null;
     echo $renderer->render(array(
         'service_link'      => add_query_arg(array('task' => 'edit-product', 'post_id' => $booking->getServiceId()), $permalink),
-        'payment_link'      => add_query_arg(array('task' => 'edit-order', 'order_id' => $booking->getPaymentId()), $permalink),
+        'payment_link'      => $paymentLink,
         'customer_link'     => null,
         'view_details_link' => null,
         'table_class'       => 'table fes-table table-condensed table-striped',
