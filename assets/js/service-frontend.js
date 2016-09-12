f71216fd5948f863c8d03a5ae84d9617e681da7a
Miguel Muscat <miguelmuscat93@gmail.com>
2016-09-12 14:38:58 +0200

    JS scopes non-body elems with "content" ID or class (EDDBOOK-101)
    EDDBOOK-101 #time 10m

--- assets/js/service-frontend.js
+++ assets/js/service-frontend.js
@@ -770,7 +770,7 @@
         var instances = {};
         $('.edd-bk-service-container').each(function (i, elem) {
             // If in content, initialize
-            if ($(elem).parents('#content, .content').length > 0) {
+            if ($(elem).parents('[id*="content"]:not(body), [class*="content"]:not(body)').length > 0) {
                 var instance = new BookableDownload(elem);
                 if (instance.id !== null) {
                     instances[i] = instance;
