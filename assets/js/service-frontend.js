2494dfdb44c8bee7398372d6e018c79806faaa31
Miguel Muscat <miguelmuscat93@gmail.com>
2016-09-13 11:39:37 +0200

    Removed widget check for calendars (EDDBOOK-103)
    EDDBOOK-103 #time 10m

--- assets/js/service-frontend.js
+++ assets/js/service-frontend.js
@@ -777,8 +777,7 @@
         var instances = {};
         $('.edd-bk-service-container').each(function (i, elem) {
             // If in content and not in a widget, initialize
-            if ($(elem).parents('[id*="content"]:not(body), [class*="content"]:not(body)').length &&
-                !$(elem).parents('[id*="widget"], [class*="widget"]').length) {
+            if ($(elem).parents('[id*="content"]:not(body), [class*="content"]:not(body)').length > 0) {
                 var instance = new BookableDownload(elem);
                 if (instance.id !== null) {
                     instances[i] = instance;
