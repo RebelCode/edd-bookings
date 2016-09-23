825c6ac5b89e4b6f10b917202649758fd2877acf
Miguel Muscat <miguelmuscat93@gmail.com>
2016-09-23 16:30:12 +0200

    Fixed undefined deactivate function if no vendor dir (EDDBOOK-107)
    EDDBOOK-107 #time 5m

--- edd-bookings.php
+++ edd-bookings.php
@@ -114,6 +114,8 @@ if (version_compare(PHP_VERSION, EDD_BK_MIN_PHP_VERSION, '<')) {
 
 // Check if vendor dir and autoload file exist
 if (!is_dir(EDD_BK_VENDOR_DIR) || !file_exists(EDD_BK_AUTOLOAD_FILE)) {
+    // load plugins.php file from WordPress if not loaded
+    require_once(ABSPATH . 'wp-admin/includes/plugin.php');
     deactivate_plugins(__FILE__);
     wp_die(
         sprintf("Plugin dependencies are not installed!<br/>"
