3ab1157ecb721c862e65803e78692db0c33e9abc
Miguel Muscat <miguelmuscat93@gmail.com>
2016-09-27 18:43:18 +0200

    Added settings config file	(EDDBOOK-109)
    EDDBOOK-109 #time 30m

---
+++ config/settings.php
@@ -0,0 +1,31 @@
+<?php
+
+// Do not edit these lines unless you know what you're doing
+use \Aventura\Edd\Bookings\Settings\Option\Option;
+use \Aventura\Edd\Bookings\Settings\Section\Section;
+
+/*
+ * Settings
+ *
+ * Adding new settings from this file involves manually creating Section or Option instances.
+ * Format:
+ *  Sections:
+ *      new Section(id, name, description, [ option1, option2, ... optionN ])
+ *  Options:
+ *      new Option(id, name, description)
+ *
+ *  Lists (notice the commas at the end):
+ *      section,
+ *      section,
+ *      section
+ *  The last item in a list should _not_ have a comma. This applies to both section lists and option lists.
+ *
+ * Example:
+    new Section('test', __('Test Section', 'eddbk'), [
+       new Option('sample_option', __('Sample Option', 'eddck'), __('This is a sample option', 'eddbk'))
+    ])
+ */
+
+return [
+    // Settings
+];
