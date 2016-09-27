0c16bfdbb03d429c382b6c40452d73677429949a
Miguel Muscat <miguelmuscat93@gmail.com>
2016-09-27 18:40:34 +0200

    Added `Option`	(EDDBOOK-109)
    EDDBOOK-109 #time 30

---
+++ includes/Settings/Option/Option.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Aventura\Edd\Bookings\Settings\Option;
+
+use \Aventura\Edd\Bookings\Settings\Database\Record\RecordInterface;
+
+/**
+ * Concrete implementation of a standard option.
+ *
+ * This class only serves a baseline - it performs no sanitization on input data.
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+class Option extends AbstractOption
+{
+
+    /**
+     * The default view name.
+     */
+    const DEFAULT_VIEW = 'Settings.Option.Default';
+
+    /**
+     * Constructs a new instance.
+     *
+     * @param string $id The option ID.
+     * @param string $name The option name.
+     * @param string $description [optional] The option description. Default = ''.
+     * @param string $view [optional] The option view name. Defaults to {@link Option::DEFAULT_VIEW}.
+     */
+    public function __construct($id, $name, $description = '', $view = null)
+    {
+        $this->setId($id)
+            ->setName($name)
+            ->setDescription($description)
+            ->setView(is_null($view)? static::DEFAULT_VIEW : $view)
+            ->setRecord(null)
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function sanitize($input)
+    {
+        return $input;
+    }
+
+}
