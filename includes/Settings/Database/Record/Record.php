9ef861166ca0aa1fe10a6dd89d59e2a4c6632398
Miguel Muscat <miguelmuscat93@gmail.com>
2016-09-27 18:39:20 +0200

    Added `Record` (EDDBOOK-109)
    EDDBOOK-109 #time 30m

---
+++ includes/Settings/Database/Record/Record.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Aventura\Edd\Bookings\Settings\Database\Record;
+
+use \Aventura\Edd\Bookings\Settings\Database\DatabaseInterface;
+
+/**
+ * Implementation of a database regular table record.
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+class Record extends AbstractRecord
+{
+
+    /**
+     * The table where the record is stored.
+     *
+     * @var DatabaseInterface
+     */
+    protected $db;
+
+    /**
+     * Constructs a new instance.
+     *
+     * @param DatabaseInterface $table The table instance.
+     * @param string $key The option key.
+     */
+    public function __construct(DatabaseInterface $table, $key)
+    {
+        $this->setDatabase($table)
+            ->setKey($key);
+    }
+
+    /**
+     * Gets the database controller instance.
+     *
+     * @return DatabaseInterface The database controller instance.
+     */
+    public function getDatabase()
+    {
+        return $this->db;
+    }
+
+    /**
+     * Sets the DB controller instance.
+     *
+     * @param DatabaseInterface $db The database controller instance.
+     * @return Record This instance.
+     */
+    public function setDatabase(DatabaseInterface $db)
+    {
+        $this->db = $db;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getValue()
+    {
+        return $this->getDatabase()->get($this->getKey());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setValue($value)
+    {
+        return $this->getDatabase()->set($this->getKey(), $value);
+    }
+
+}
