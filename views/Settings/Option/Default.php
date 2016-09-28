d703fb0a7cf47acf6d9a49df3d463c1fb5d80293
Miguel Muscat <miguelmuscat93@gmail.com>
2016-09-28 17:15:20 +0200

    Updated default option view (EDDBOOK-109)
    EDDBOOK-109 #time 15m

--- views/Settings/Option/Default.php
+++ views/Settings/Option/Default.php
@@ -1,5 +1,7 @@
 <label>
-    This option is not yet implemented. This message is being generated from the
-    <code>Settings.Option.Default</code>
-    view.
-</label>
+    <?php echo $data['desc']; ?>
+<label>
+
+<p>
+    <strong>This option has not yet been implemented. This message is being generated from the <code>Settings.Option.Default</code> view.</strong>
+</p>
