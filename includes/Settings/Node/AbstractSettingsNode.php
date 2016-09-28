7339d46425024d1d13a850a9f0410d46b05e9bdd
Miguel Muscat <miguelmuscat93@gmail.com>
2016-09-28 14:30:00 +0200

    Added `AbstractSettingsNode` (EDDBOOK-109)
    EDDBOOK-109 #time 15m

---
+++ includes/Settings/Node/AbstractSettingsNode.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Aventura\Edd\Bookings\Settings\Node;
+
+use \Aventura\Edd\Bookings\Settings\Database\Record\RecordInterface;
+
+/**
+ * Description of AbstractSettingsNode
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+class AbstractSettingsNode implements SettingsNodeInterface
+{
+
+    /**
+     * The ID.
+     *
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * The name.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * The description.
+     *
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * The record instance.
+     *
+     * @var RecordInterface
+     */
+    protected $record;
+
+    /**
+     * The view name.
+     *
+     * @var string
+     */
+    protected $view;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRecord()
+    {
+        return $this->record;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getView()
+    {
+        return $this->view;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setRecord(RecordInterface $record = null)
+    {
+        $this->record = $record;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setView($view)
+    {
+        $this->view = $view;
+        return $this;
+    }
+
+}
