99e501528e04ab962080d3b463cca23713c1cf31
Miguel Muscat <miguelmuscat93@gmail.com>
2016-09-28 17:15:44 +0200

    Updated settings config integration for XML (EDDBOOK-109)
    EDDBOOK-109 #time 25m

--- integrations/settings-config.php
+++ integrations/settings-config.php
@@ -1,12 +1,38 @@
 <?php
 
+use \Aventura\Edd\Bookings\Settings\Option\Option;
+use \Aventura\Edd\Bookings\Settings\Section\Section;
+
 /**
  * A simple interal integration that loads a configuration file of sections and options for the Plugin's settings.
  */
 
-// Load config file
-$config = eddBookings()->loadConfigFile('settings');
-// If valid, register
-if (is_array($config)) {
-    eddBookings()->getSettings()->addSections($config);
+// Settings instance
+$settings = eddBookings()->getSettings();
+
+// Load the XML file
+$xml = simplexml_load_file(EDD_BK_CONFIG_DIR . 'settings.xml');
+// Iterate sections
+foreach($xml as $sectionNode) {
+    // Create a section instance
+    $sectionAttrs = $sectionNode->attributes();
+    $section = new Section(
+        (string) $sectionAttrs['id'],
+        (string) $sectionAttrs['name']
+    );
+    // Register the section
+    $settings->addSection($section);
+    // Iterate options
+    foreach ($sectionNode->children() as $optionNode) {
+        // Create the option instance
+        $optionAttrs = $optionNode->attributes();
+        $option = new Option(
+            (string) $optionAttrs['id'],
+            (string) $optionAttrs['name'],
+            (string) $optionNode,
+            (string) $optionAttrs['view']
+        );
+        // Register the option
+        $section->addOption($option);
+    }
 }
