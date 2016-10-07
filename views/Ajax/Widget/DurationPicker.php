8bd2b4b023983e9ce046552ea1a8888ce4b0986b
Miguel Muscat <miguelmuscat93@gmail.com>
2016-10-07 18:40:19 +0200

    Added `DurationPicker` static alt element (EDDBOOK-58)
    EDDBOOK-58 #time 10m

--- views/Ajax/Widget/DurationPicker.php
+++ views/Ajax/Widget/DurationPicker.php
@@ -1,4 +1,5 @@
 <div class="edd-bk-widget edd-bk-duration-picker">
     <input type="number" class="edd-bk-duration-picker-field" />
+    <span class="edd-bk-duration-picker-static-alt"></span>
     <span class="edd-bk-duration-picker-unit"></span>
 </div>
