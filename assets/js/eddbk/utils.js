1e964c637d07070a73345b63a5372cbbfddf7557
Miguel Muscat <miguelmuscat93@gmail.com>
2016-10-10 11:24:36 +0200

    Added `EddBk.Utils.isTimeUnit()` function (EDDBOOK-58)
    EDDBOOK-58 #time 10m

--- assets/js/eddbk/utils.js
+++ assets/js/eddbk/utils.js
@@ -10,13 +10,17 @@
             days: 'days',
             weeks: 'weeks'
         },
-         // Time units and their respective lengths in seconds
+        // Time units and their respective lengths in seconds
         UnitLengths: {
             minutes: 60,
             hours: 3600,
             days: 86400,
             weeks: 604800
-        }
+        },
+        // Checks if the argument string is a time unit string name
+        isTimeUnit: function(something) {
+            return [EddBk.Utils.Units.hours, EddBk.Utils.Units.minutes].indexOf(something) !== -1;
+        },
     };
 
 })(jQuery, top, document, EddBkAjaxLocalized);
