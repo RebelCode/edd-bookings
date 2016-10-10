ea82bb50fe4f627c5e13183aa7368f82b3ad9cd1
Miguel Muscat <miguelmuscat93@gmail.com>
2016-10-10 13:29:47 +0200

    Added `EddBk.Object.Session` JS Class (EDDBOOK-58)
    EDDBOOK-58 #time 15m

---
+++ assets/js/eddbk/object/session.js
@@ -0,0 +1,64 @@
+/* global EddBk, top */
+
+;(function ($, window, document, undefined) {
+
+    EddBk.newClass('EddBk.Object.Session', EddBk.Object, {
+        init: function(start, duration) {
+            this.super(
+                $.extend(this.getDefaults(), {
+                    start: start,
+                    duration: duration
+                })
+            );
+            this._init();
+        },
+        /**
+         * To be implemented by extending classes.
+         */
+        _init: function() {},
+        /**
+         * Gets the session starting date and time.
+         *
+         * @returns {Date} The date object instance.
+         */
+        getStart: function() {
+            return this.getData('start');
+        },
+        /**
+         * Gets the session duration.
+         *
+         * @returns {integer} The number of seconds.
+         */
+        getDuration: function() {
+            return this.getData('duration');
+        },
+        /**
+         * Sets the session starting date and time.
+         *
+         * @param {Date} start The date object instance.
+         */
+        setStart: function(start) {
+            this.setData('start', start);
+        },
+        /**
+         * Sets the session duration.
+         *
+         * @param {integer} duration The number of seconds.
+         */
+        setDuration: function(duration) {
+            this.setData('duration', duration);
+        },
+        /**
+         * Gets the default data.
+         *
+         * @returns {object}
+         */
+        getDefaults: function () {
+            return {
+                start: null,
+                duration: null
+            };
+        }
+    });
+
+})(jQuery, top, document);
