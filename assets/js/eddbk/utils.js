d5d5fd7ff0330e6aab66c1a3fd0f1def90aa75c1
Miguel Muscat <miguelmuscat93@gmail.com>
2016-10-11 17:53:14 +0200

    Added UTC timestamp util methods in `EddBk.Utils` (EDDBOOK-58)
    EDDBOOK-58 #time 10m

--- assets/js/eddbk/utils.js
+++ assets/js/eddbk/utils.js
@@ -21,6 +21,15 @@
         isTimeUnit: function(something) {
             return [EddBk.Utils.Units.hours, EddBk.Utils.Units.minutes].indexOf(something) !== -1;
         },
+        // Converts milliseconds into seconds, i.e. a proper timestamp
+        msToSeconds: function(ms) {
+            return Math.floor(ms / 1000);
+        },
+        // Creates a UTC timestamp
+        utcTimestamp: function(year, month, date, hours, minutes, seconds, ms) {
+            var d = Date.UTC(year, month, date, hours || 0, minutes || 0, seconds || 0, ms || 0);
+            return EddBk.Utils.msToSeconds(d);
+        }
     };
 
 })(jQuery, top, document, EddBkAjaxLocalized);
