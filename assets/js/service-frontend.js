7c6190379ef994602851e7b9ed47d15ae0678de8
Miguel Muscat <miguelmuscat93@gmail.com>
2016-10-24 13:35:15 +0200

    Refactored service-frontend.js into a class (EDDBOOK-58)
    EDDBOOK-58 #time 4h 35m

--- assets/js/service-frontend.js
+++ assets/js/service-frontend.js
@@ -1,76 +1,138 @@
 /* global edd_scripts */
 
-(function ($) {
+(function ($, undefined) {
 
-    function saveEddHandler() {
-        // Get the EDD click handler function
-        var eddHandler = $('body').data('events')['click.eddAddToCart'];
-        // For more recent jquery versions:
-        if (!eddHandler) {
-            // Get all click bindings
-            var bindings = $._data(document.body, 'events')['click'];
-            // Search all bindings for those with the 'eddAddToCart' namespace
-            for (var i in bindings) {
-                if (bindings[i].namespace === 'eddAddToCart') {
-                    eddHandler = bindings[i].handler;
-                    break;
-                }
-            }
-        }
-        // Set globals
-        window.eddBkGlobals = {
-            eddHandler: eddHandler
-        };
-    }
+    EddBk.newClass('EddBk.PurchaseForm', EddBk.Object, {
+        // Initializes the instance
+        init: function (element) {
+            this.l = element;
+            // Init service and session picker
+            this.service = new EddBk.Service(this.l.data('service'));
+            this.sessionPicker = new EddBk.Widget.ServiceSessionPicker(this.l, this.service);
+            // Instance loads content after it loads the service data
+            this.sessionPicker.loadData(this.sessionPicker.loadContent.bind(this.sessionPicker));
 
-    function createInstance(elem) {
-        var instance = new EddBkPurchaseFormSessionPicker(elem);
-        instance.init();
-        return instance;
-    }
+            this.initElements();
+            this.initEvents();
+        },
+        // Initializes the element pointers
+        initElements: function () {
+            this.form = this.l.closest('form');
+            this.btn = this.form.find('.edd-add-to-cart:visible');
 
-    function createInstances(selector) {
-        // Initialize the instances
-        window.eddBkInstances = {};
-        // Find all elements that match the selector
-        $(selector).each(function (i, elem) {
-            var $elem = $(elem);
-            // If in content and not in a widget, initialize
-            if ($elem.parents('[id*="content"]:not(body), [class*="content"]:not(body)').length > 0) {
-                var instance = createInstance($elem);
-                if (instance !== null) {
-                    window.eddBkInstances[i] = instance;
-                }
+            return this;
+        },
+        // Initializes the events
+        initEvents: function () {
+            // Show/hide the add to cart button depending on the validity of the selected session
+            this.sessionPicker.on('update', this.onUpdate.bind(this));
+
+            // Get the closest form and its purchase button
+            var form = this.l.closest('form'),
+                addToCartBtn = form.find('.edd-add-to-cart:visible');
+
+            addToCartBtn.click(this.onSubmit.bind(this));
+            form.on('submit', this.onSubmit.bind(this));
+
+            return this;
+        },
+        // Triggered on update
+        onUpdate: function () {
+            this.btn.toggle(this.sessionPicker.getSelectedSession() !== null);
+        },
+        // Triggered on submission
+        onSubmit: function (ev) {
+            ev.preventDefault();
+            ev.stopPropagation();
+
+            // Update spinner
+            var $spinner = this.btn.find('.edd-loading');
+            $spinner.css({
+                'margin-left': $spinner.width() / -2,
+                'margin-top': $spinner.height() / -2
+            });
+            // Disable button, preventing rapid additions to cart during ajax request and show the spinner
+            this.btn
+                .prop('disabled', true)
+                .attr('data-edd-loading', '');
+
+            this.sessionPicker.validate(this.onValidateSession.bind(this, ev));
+        },
+        // Triggered when a session is validated with the server when the form is submitted
+        onValidateSession: function (ev, valid) {
+            if (valid) {
+                // Invoke default EDD ATC handler function
+                EddBk.PurchaseForm.getEddAddToCartEventHandler().apply(this.btn, [ev]);
             } else {
-                // Otherwise, remove element
-                $elem.remove();
-                return;
+                // Re-enable the button
+                // If not valid, the session picker will show the "unavailable session" message.
+                this.btn.removeAttr('data-edd-loading')
+                    .prop('disabled', false);
             }
-        });
-
-        return window.eddBkInstances;
-    }
-
-    $(document).ready(function () {
-        return;
-        // Save the EDD add to cart handler
-        saveEddHandler();
-        // Initialize the instances
-        var instances = createInstances('.edd-bk-service-container');
-        // If instances have been created, unbind the traditional EDD add to cart handler.
-        if (Object.keys(instances).length) {
-            $('body').unbind('click.eddAddToCart');
         }
     });
 
-    $(document).ready(function() {
-        $('.content-area .edd-bk-service-session-picker').each(function(i, l) {
-            var serviceId = $(l).data('service');
-            var service = new EddBk.Service(serviceId);
-            var instance = new EddBk.Widget.ServiceSessionPicker(l, service);
-            instance.loadData(instance.loadContent.bind(instance));
-        });
+    /**
+     * Utility static method for getting the EDD ATC handler function.
+     * 
+     * @returns {Function}
+     */
+    EddBk.PurchaseForm.getEddAddToCartEventHandler = function() {
+        if (EddBk.PurchaseForm.eddAddToCartEventHandler === undefined) {
+            // Get the EDD click handler function
+            var eventType = 'click',
+                eventNamespace = 'eddAddToCart',
+                eventHandle = eventType + '.' + eventNamespace,
+                eddHandler = $('body').data('events')[eventHandle];
+            // For more recent jquery versions:
+            if (!eddHandler) {
+                // Get all click bindings
+                var bindings = $._data(document.body, 'events')[eventType];
+                // Search all bindings for those with the 'eddAddToCart' namespace
+                for (var i in bindings) {
+                    if (bindings[i].namespace === eventNamespace) {
+                        eddHandler = bindings[i].handler;
+                        break;
+                    }
+                }
+            }
+            // Save pointer to handler
+            EddBk.PurchaseForm.eddAddToCartEventHandler = eddHandler;
+            // Unbind all handlers from the event
+            $('body').unbind(eventHandle);
+        }
+
+        return EddBk.PurchaseForm.eddAddToCartEventHandler;
+    };
+
+    // Initializes instances when the DOM is ready
+    $(document).ready(function () {
+        // Create and save instances
+        EddBk.PurchaseForm.instances = autoCreateInstances($('.edd-bk-service-session-picker'));
     });
 
+    /**
+     * Creates all the required session picker widget instances for the current page.
+     *
+     * @param {jQuery} jq The jQuery element selector object.
+     * @returns {Array} An array of created instances.
+     */
+    function autoCreateInstances(jq) {
+        var instances = [];
+
+        // Create instances
+        jq.each(function (i, l) {
+            var $l = $(l);
+            // If in content and not in a widget, initialize
+            if ($l.parents('[id*="content"]:not(body), [class*="content"]:not(body)').length > 0) {
+                var instance = new EddBk.PurchaseForm($l);
+                instances.push(instance);
+            } else {
+                $l.remove();
+            }
+        });
+
+        return instances;
+    }
 
 })(jQuery);
