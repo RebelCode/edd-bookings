174bec79e9b9e14543b95e80f81a3627839db4d7
Miguel Muscat <miguelmuscat93@gmail.com>
2016-10-27 18:24:38 +0200

    CartRenderer now renders one of two views (EDDBOOK-110)
    - BookingSession view used for cart items with sessions
    - NoSessions view used for cart items without sessiosn
    
    EDDBOOK-110 #time 40m

---
+++ views/Frontend/Cart/Item/BookingSession.php
@@ -0,0 +1,8 @@
+<p class="edd-bk-cart-booking-start">
+    <strong><?php echo _x('From:', 'From [date and time] till [date and time]', 'eddbk'); ?></strong>
+    <em><?php echo $data['start']; ?></em>
+</p>
+<p class="edd-bk-cart-booking-end">
+    <strong><?php echo _x('Till:', 'From [date and time] till [date and time]', 'eddbk'); ?></strong>
+    <em><?php echo $data['end']; ?></em>
+</p>
---
+++ views/Frontend/Cart/Item/NoSession.php
@@ -0,0 +1,8 @@
+<hr class="edd-bk-hr" />
+
+<div class="edd-bk-service-session-picker"></div>
+
+<a href="#" class="edd-bk-edit-cart-item-session edd-add-to-cart button blue edd-submit edd-has-js">
+    <span class="edd-add-to-cart-label"><?php _e('Choose Session', 'eddbk'); ?></span>
+    <span class="edd-loading"><i class="edd-icon-spinner edd-icon-spin"></i></span>
+</a>
