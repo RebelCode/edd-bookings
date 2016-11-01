b05fa660f1a59f61c7fea1092ccc052ec379ff61
Miguel Muscat <miguelmuscat93@gmail.com>
2016-11-01 12:29:28 +0100

    NoSession view only outputs a link

--- views/Frontend/Cart/Item/NoSession.php
+++ views/Frontend/Cart/Item/NoSession.php
@@ -1,8 +1,7 @@
-<hr class="edd-bk-hr" />
-
-<div class="edd-bk-service-session-picker"></div>
-
-<a href="#" class="edd-bk-edit-cart-item-session edd-add-to-cart button blue edd-submit edd-has-js">
-    <span class="edd-add-to-cart-label"><?php _e('Choose Session', 'eddbk'); ?></span>
-    <span class="edd-loading"><i class="edd-icon-spinner edd-icon-spin"></i></span>
+<?php
+$i = $data['index'];
+?>
+ |
+<a href="javascript:void(0)" data-cart-index="<?= $i ?>" class="edd-bk-show-modal-<?= $i ?>">
+    <?php _e('Choose session', 'eddbk'); ?>
 </a>
