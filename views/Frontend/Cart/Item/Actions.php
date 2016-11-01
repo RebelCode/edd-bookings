5e05563d85a3f803f569b5b5ee34a0d8b8e987f7
Miguel Muscat <miguelmuscat93@gmail.com>
2016-11-01 14:05:03 +0100

    Changed "Edit Session" action text

--- views/Frontend/Cart/Item/Actions.php
+++ views/Frontend/Cart/Item/Actions.php
@@ -1,3 +1,3 @@
 <a href="javascript:void(0)" class="edd-bk-cart-edit-action" id="edd-bk-cart-edit-item-<?= $data['index'] ?>">
-    <?php _e('Edit Booking Session', 'eddbk'); ?>
+    <?php _e('Edit Session', 'eddbk'); ?>
 </a>
