7629b3aa3ff0e77796d2ce3c3285ecb339e2569f
Miguel Muscat <miguelmuscat93@gmail.com>
2016-11-10 16:24:15 +0100

    Refactored service-edit.js
    GitHub Issue #6
    EDDBOOK-69 #time 30m

--- assets/js/service-edit.js
+++ assets/js/service-edit.js
@@ -1,29 +1,31 @@
-function eddBkService(element) {
-    $ = jQuery;
-    element = $(element);
-    
-    // Toggles the sections based on whether bookings are enabled
-    var updateSectionVisibility = function() {
-        var bookingsEnabled = element.find('input#edd-bk-bookings-enabled').is(':checked');
-        element.find('div.edd-bk-collapse-container').toggle(bookingsEnabled);
-        // Also hide some other metaboxes
-        var edd_metaboxes_to_hide = [
-            '#edd_product_prices',
-            '#edd_product_files'
-        ];
-        $(edd_metaboxes_to_hide.join(',')).toggle(!bookingsEnabled);
-    };
-    
-    // When the bookings are enabled/disabled, update the section visibility
-    element.find('input#edd-bk-bookings-enabled').change(updateSectionVisibility);
-    
-    // Check section visibility on first run
-    updateSectionVisibility();
-}
+;(function($, window, document, undefined) {
+    function eddBkService(element) {
+        element = $(element);
 
-// Initializes all service containers
-jQuery(document).ready(function() {
-    jQuery('div.edd-bk-service-container').each(function() {
-        eddBkService(this);
+        // Toggles the sections based on whether bookings are enabled
+        var updateSectionVisibility = function() {
+            var bookingsEnabled = element.find('input#edd-bk-bookings-enabled').is(':checked');
+            element.find('div.edd-bk-collapse-container').toggle(bookingsEnabled);
+            // Also hide some other metaboxes
+            var edd_metaboxes_to_hide = [
+                '#edd_product_prices',
+                '#edd_product_files'
+            ];
+            $(edd_metaboxes_to_hide.join(',')).toggle(!bookingsEnabled);
+        };
+
+        // When the bookings are enabled/disabled, update the section visibility
+        element.find('input#edd-bk-bookings-enabled').change(updateSectionVisibility);
+
+        // Check section visibility on first run
+        updateSectionVisibility();
+    }
+
+    // Initializes all service containers
+    $(document).ready(function() {
+        $('div.edd-bk-service-container').each(function() {
+            eddBkService(this);
+        });
     });
-});
+
+})(jQuery, top, document);
