284d2df5e34134f30766ad7976ef7de65ed85984
Miguel Muscat <miguelmuscat93@gmail.com>
2016-11-11 16:55:49 +0100

    Refactored service-edit for flexible extending
    GitHub Issue #6
    EDDBOOK-69 #time 1h

--- assets/js/service-edit.js
+++ assets/js/service-edit.js
@@ -1,30 +1,77 @@
 ;(function($, window, document, undefined) {
-    function eddBkService(element) {
-        element = $(element);
 
-        // Toggles the sections based on whether bookings are enabled
-        var updateSectionVisibility = function() {
-            var bookingsEnabled = element.find('input#edd-bk-bookings-enabled').is(':checked');
-            element.find('div.edd-bk-collapse-container').toggle(bookingsEnabled);
-            // Also hide some other metaboxes
-            var edd_metaboxes_to_hide = [
-                '#edd_product_prices',
-                '#edd_product_files'
-            ];
-            $(edd_metaboxes_to_hide.join(',')).toggle(!bookingsEnabled);
-        };
+    // Service instance for the service currently being edited
+    var service = new EddBk.Service(-15),
+        l_bookingsEnabled = null,
+        l_sessionLength = null,
+        l_sessionUnit = null,
+        l_minSessions = null,
+        l_maxSessions = null,
+        l_sessionCost = null,
+        l_useCustomerTz = null,
+        availBuilder = null,
+        eddMetaBoxesToHide = [
+            '#edd_product_prices',
+            '#edd_product_files'
+        ];
 
-        // When the bookings are enabled/disabled, update the section visibility
-        element.find('input#edd-bk-bookings-enabled').change(updateSectionVisibility);
-
-        // Check section visibility on first run
-        updateSectionVisibility();
+    /**
+     * Initializes the element pointers.
+     */
+    function initElements() {
+        l_bookingsEnabled = $('#edd-bk-bookings-enabled');
+        l_sessionLength = $('#edd-bk-session-length');
+        l_sessionUnit = $('#edd-bk-session-unit');
+        l_minSessions = $('#edd-bk-min-sessions');
+        l_maxSessions = $('#edd-bk-max-sessions');
+        l_sessionCost = $('#edd-bk-session-cost');
+        l_useCustomerTz = $('#edd-bk-use-customer-tz');
+        availBuilder = $('#edd-bk-service.postbox .edd-bk-availability-container');
     }
 
-    // Initializes all service containers
+    // Toggles the sections based on whether bookings are enabled
+    function updateSectionVisibility(container) {
+        var bookingsEnabled = l_bookingsEnabled.is(':checked');
+        container.find('div.edd-bk-collapse-container').toggle(bookingsEnabled);
+        $(eddMetaBoxesToHide.join(',')).toggle(!bookingsEnabled);
+    }
+
+    /**
+     * Initializes a service container section.
+     *
+     * @param {Element} container The container section element.
+     */
+    function initServiceContainerSection(container) {
+        container = $(container);
+
+        // When the bookings are enabled/disabled, update the section visibility
+        l_bookingsEnabled.change(updateSectionVisibility.bind(null, container));
+
+        // Check section visibility on first run
+        updateSectionVisibility(container);
+    }
+
+    /**
+     * Updates the service instance based on the current values of the form fields.
+     */
+    function updateServiceInstance() {
+        service.addData({
+            session_unit: l_sessionUnit.val(),
+            session_length_n: parseInt(l_sessionLength.val()),
+            session_length: parseInt(l_sessionLength.val()) * EddBk.Utils.UnitLengths[l_sessionUnit.val()],
+            min_sessions: parseInt(l_minSessions.val()),
+            max_sessions: parseInt(l_maxSessions.val()),
+            session_cost: parseFloat(l_sessionCost.val()),
+            use_customer_tz: l_useCustomerTz.is(':checked')
+        });
+    }
+
     $(document).ready(function() {
+        initElements();
+
+        // Initializes all service containers
         $('div.edd-bk-service-container').each(function() {
-            eddBkService(this);
+            initServiceContainerSection(this);
         });
     });
 
