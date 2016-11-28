2d806dd7ced7fd71fc0b33bd95cb59acf6242342
Miguel Muscat <miguelmuscat93@gmail.com>
2016-11-28 14:39:05 +0100

    Moved service info msg under Service list
    Issue #7, EDDBOOK-47

--- views/Admin/Bookings/Edit.php
+++ views/Admin/Bookings/Edit.php
@@ -44,6 +44,45 @@ $serverTz = eddBookings()->getServerTimezoneOffsetSeconds();
         ));
         ?>
     </div>
+    <div>
+        <p id="service-info-loading">
+            <i class="fa fa-spinner fa-spin"></i>
+            <?php _e('Getting service information ...', 'eddbk'); ?>
+        </p>
+        <p id="service-info-msg-singular" class="info-msg">
+            <i class="fa fa-info-circle"></i>
+            <?php
+                printf(
+                    _x(
+                        'The %1$s service uses a session length of %2$s %3$s and customers can book %4$s session(s).',
+                        'Example: The Bike Renta; service uses a session length of 30 minutes and cusomters can book 3 session(s).',
+                        'eddbk'
+                    ),
+                    '<span class="service-name"></span>',
+                    '<span class="session-length"></span>',
+                    '<span class="session-unit"></span>',
+                    '<span class="num-sessions"></span>'
+                );
+            ?>
+        </p>
+        <p id="service-info-msg-plural" class="info-msg">
+            <i class="fa fa-info-circle"></i>
+            <?php
+                printf(
+                    _x(
+                        'The %1$s service uses a session length of %2$s %3$s and customers can book between %4$s and %5$s sessions.',
+                        'Example: The Bike Renta; service uses a session length of 2 hours and cusomters can book between 1 and 2 sessions',
+                        'eddbk'
+                    ),
+                    '<span class="service-name"></span>',
+                    '<span class="session-length"></span>',
+                    '<span class="session-unit"></span>',
+                    '<span class="min-sessions"></span>',
+                    '<span class="max-sessions"></span>'
+                );
+            ?>
+        </p>
+    </div>
 
     <div>
         <label for="payment">
@@ -103,43 +142,6 @@ $serverTz = eddBookings()->getServerTimezoneOffsetSeconds();
     <hr/>
 
     <h4><?php _e('Booking Details', 'eddbk'); ?></h4>
-    <div>
-        <p id="service-info-loading">
-            <i class="fa fa-spinner fa-spin"></i>
-            <?php _e('Getting service information ...', 'eddbk'); ?>
-        </p>
-        <p id="service-info-msg-singular" class="info-msg">
-            <?php
-                printf(
-                    _x(
-                        'The %1$s service uses a session length of %2$s %3$s and customers can book %4$s session(s).',
-                        'Example: The Bike Renta; service uses a session length of 30 minutes and cusomters can book 3 session(s).',
-                        'eddbk'
-                    ),
-                    '<span class="service-name"></span>',
-                    '<span class="session-length"></span>',
-                    '<span class="session-unit"></span>',
-                    '<span class="num-sessions"></span>'
-                );
-            ?>
-        </p>
-        <p id="service-info-msg-plural" class="info-msg">
-            <?php
-                printf(
-                    _x(
-                        'The %1$s service uses a session length of %2$s %3$s and customers can book between %4$s and %5$s sessions.',
-                        'Example: The Bike Renta; service uses a session length of 2 hours and cusomters can book between 1 and 2 sessions',
-                        'eddbk'
-                    ),
-                    '<span class="service-name"></span>',
-                    '<span class="session-length"></span>',
-                    '<span class="session-unit"></span>',
-                    '<span class="min-sessions"></span>',
-                    '<span class="max-sessions"></span>'
-                );
-            ?>
-        </p>
-    </div>
     <div>
         <label for="start">
             <span>
