db4364ff14c2faa1d080b05a2d789e71d11f3a69
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-01 18:07:41 +0100

    Updated "What's New" About page

--- views/about/index.php
+++ views/about/index.php
@@ -1,8 +1,23 @@
+<div class="headline-feature feature-section one-col">
+    <center>
+        <h2><?php _e('Version Highlights', 'eddbk'); ?></h2>
+        <p class="eddbk-small"><?php _e("Some clever subtitle here", 'eddbk'); ?></p>
+    </center>
+</div>
+
 <div class="headline-feature feature-section two-col">
     <div class="col">
-        <h2><?php _e('Manual Booking Handling', 'eddbk'); ?></h2>
+        <h3><?php _e('Manual Booking Handling', 'eddbk'); ?></h3>
         <p>
-            <?php _e('You can now create and edit bookings from the backend.', 'eddbk'); ?>
+            <?php _e('EDD Bookings 2.2.0 introduced the ability to manually manage your bookings directly through your WordPress Admin.', 'eddbk'); ?>
+        </p>
+        <p>
+            <?php _e('You can now editing existing bookings, allowing you to change the booking times, the service provided, the customer and more.', 'eddbk'); ?>
+            <?php _e("You can also distinguish between confirmed and draft bookings so you can save your changes without commitment for later editing.", 'eddbk'); ?>
+        </p>
+        <p>
+            <?php _e("Adding new bookings manually is a similar process.", 'eddbk');?>
+            <?php _e("If you accept bookings from other channels, such as through the phone or in person, you can now register them in EDD Bookings to disallow further bookings for those dates and times ", 'eddbk'); ?>
         </p>
     </div>
     <div class="col">
@@ -14,9 +29,31 @@
 
 <div class="headline-feature feature-section two-col">
     <div class="col">
-        <h2><?php _e('Availability Preview', 'eddbk'); ?></h2>
+        <div class="media-container">
+            <img src="<?php echo EDD_BK_IMGS_URL; ?>fes-integration.png" />
+        </div>
+    </div>
+    <div class="col">
+        <h3><?php _e('Availability Preview', 'eddbk'); ?></h3>
         <p>
-            <?php _e('The new availability preview lets you preview your availability while you edit it.', 'eddbk'); ?>
+            <?php _e("The Availability Preview feature lets you build your Downloads' availabilities easier by giving you the ability to preview the calendar shown on your site.", 'eddbk'); ?>
+        </p>
+        <p>
+            <?php _e("While making changes to your availability, you can now refer to the preview to check whether or not the results are what you were expecting.", 'eddbk'); ?>
+            <?php _e("This should both save you some time and also allow you to avoid having to save the Download just to view its availability.", 'eddbk'); ?>
+        </p>
+    </div>
+</div>
+
+<div class="headline-feature feature-section two-col">
+    <div class="col">
+        <h3><?php _e('Message Customization', 'eddbk'); ?></h3>
+        <p>
+            <?php _e("Sometimes, a message shown to your site viewers does not fit your site's style of writing. And if you have no means to change such messages, they will stick out and feel disconnected.", 'eddbk'); ?>
+        </p>
+        <p>
+            <?php _e("So we've added an option to change the text of the error message shown on the checkout page when a bookable Download does not have a booked session.", 'eddbk'); ?>
+            <?php _e("We will be adding more message options in future version to ensure that you have complete control over what your viewers see on your site.", 'eddbk'); ?>
         </p>
     </div>
     <div class="col">
@@ -71,7 +108,7 @@
         <div class="col">
             <h4><?php _e('Email Notifications', 'eddbk'); ?></h4>
             <p>
-                <?php _e("Set up email notifications to remind your customers of their bookings at certain times, be it a day or a few hours before.", 'eddbk'); ?>
+                <?php _e("Set up emails to send to your customers for when a booking is purchased, when a cancellations is made or to simply remind them of their upcoming booking.", 'eddbk'); ?>
             </p>
         </div>
         <div class="col">
