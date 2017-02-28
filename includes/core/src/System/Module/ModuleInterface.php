6da79c174fef09e3eba5eae2b8df79254cbcc35b
Miguel Muscat <miguelmuscat93@gmail.com>
2017-02-28 10:22:36 +0100

    Added docs in `ModuleInterface`

--- includes/core/src/System/Module/ModuleInterface.php
+++ includes/core/src/System/Module/ModuleInterface.php
@@ -5,14 +5,34 @@ namespace RebelCode\EddBookings\System\Module;
 use \RebelCode\Bookings\Framework\Data\DataReadableInterface;
 
 /**
+ * Something that represents an application module.
  *
  * @since [*next-version*]
  */
 interface ModuleInterface extends DataReadableInterface
 {
+    /**
+     * Retrieves the module ID.
+     *
+     * @since [*next-version*]
+     *
+     * @return string The module ID.
+     */
     public function getId();
 
+    /**
+     * Retrieves the module name.
+     *
+     * @since [*next-version*]
+     *
+     * @return stirng The module name.
+     */
     public function getName();
 
+    /**
+     * Loads the module.
+     *
+     * @since [*next-version*]
+     */
     public function load();
 }
