d59b6d54c30ef9fb605f7c2928a47833a54dac0d
Miguel Muscat <miguelmuscat93@gmail.com>
2017-03-01 18:52:21 +0100

    Added `AbstractTopLevelMenu`

---
+++ includes/modules/wp-helper/src/Admin/Menu/AbstractTopLevelMenu.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace RebelCode\Wp\Admin\Menu;
+
+/**
+ * Basic functionality for a top-level menu.
+ *
+ * @since [*next-version*]
+ */
+abstract class AbstractTopLevelMenu extends AbstractMenu
+{
+}
