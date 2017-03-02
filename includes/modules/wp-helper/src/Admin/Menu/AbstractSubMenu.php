4433b3ee53e612822371814cadeb0ac3dc2c67d4
Miguel Muscat <miguelmuscat93@gmail.com>
2017-03-02 13:43:27 +0100

    Added `AbstractSubMenu#_getParentId()`

--- includes/modules/wp-helper/src/Admin/Menu/AbstractSubMenu.php
+++ includes/modules/wp-helper/src/Admin/Menu/AbstractSubMenu.php
@@ -45,4 +45,27 @@ abstract class AbstractSubMenu extends AbstractMenu
 
         return $this;
     }
+
+    /**
+     * Gets the parent ID.
+     *
+     * Since WordPress menus use their URLs as IDs if they redirect to a URL, this method
+     * will check the parent's content type and if found to be a URL string, that string
+     * will be the returned as the ID.
+     *
+     * Otherwise, if the parent content is a callback, its ID property is returned.
+     *
+     * @since [*next-version*]
+     *
+     * @return string
+     */
+    protected function _getParentId()
+    {
+        $parentMenu    = $this->_getParentMenu();
+        $parentContent = $this->_normalizeContent($parentMenu->getContent());
+
+        return filter_var($parentContent, FILTER_VALIDATE_URL)
+            ? $parentContent
+            : $parentMenu->getId();
+    }
 }
