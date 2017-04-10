9ada08fda196be4242b86948d3e6a42f4a600c29
Miguel Muscat <miguelmuscat93@gmail.com>
2017-04-10 17:45:47 +0200

    Made unavailable msg more generic (EDDBOOK-255)

--- views/Widget/ServiceSessionPicker.php
+++ views/Widget/ServiceSessionPicker.php
@@ -1,7 +1,7 @@
 <?php echo eddBookings()->renderView('Widget.SessionPicker', $data); ?>
 
 <div class="edd-bk-session-picker-msg edd-bk-session-picker-session-unavailable">
-    <?php _e('Your chosen session is unavailable. It may have been booked by someone else. If you believe this is a mistake, please contact the site administrator.', 'eddbk') ?>
+    <?php _e('Your chosen session is unavailable. If you believe this is a mistake, please contact the site administrator.', 'eddbk') ?>
 </div>
 
 <input class="edd-bk-fs-start" type="hidden" name="edd_bk_start" />
